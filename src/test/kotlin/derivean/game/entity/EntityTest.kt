201805e183648a451948ed6b15a5d0ced2ba836c
Marek Hanzal <marek.hanzal@x32.cz>
2020-05-04 15:07:02 +0200

    Game: Entity cannot attack another Entity

--- src/test/kotlin/derivean/game/entity/EntityTest.kt
+++ src/test/kotlin/derivean/game/entity/EntityTest.kt
@@ -53,36 +53,4 @@ class EntityTest {
 			}
 		}
 	}
-
-	@Test
-	fun `Get Damage from two Entities`() {
-		val gandalf = Entity.build {
-			health = 100.0
-			attack {
-				physical = 5.0
-				magical = 12.0
-			}
-			defense {
-				physical = 2.5
-				magical = 4.0
-			}
-		}
-		val aragorn = Entity.build {
-			health = 160.0
-			attack {
-				physical = 16.0
-			}
-			defense {
-				physical = 4.0
-			}
-		}
-		with(gandalf.damage(aragorn)) {
-			assertEquals(1.0, physical, "Gandalf hurts Aragorn with wrong physical damage")
-			assertEquals(12.0, magical, "Gandalf hurts Aragorn with wrong magical damage")
-		}
-		with(aragorn.damage(gandalf)) {
-			assertEquals(13.5, physical, "Aragorn hurts Gandalf with wrong physical damage")
-			assertEquals(-4.0, magical, "Aragorn hurts Gandalf with wrong magical damage")
-		}
-	}
 }
