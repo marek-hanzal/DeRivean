393b6d18301d736ff2d2162fd78d536b5df1979e
Marek Hanzal <marek.hanzal@x32.cz>
2020-05-04 14:15:02 +0200

    Game: Added simple defesne class

--- src/main/kotlin/derivean/game/entity/Entity.kt
+++ src/main/kotlin/derivean/game/entity/Entity.kt
@@ -2,7 +2,8 @@ package derivean.game.entity
 
 data class Entity(
 	val health: Double,
-	val attack: Attack
+	val attack: Attack,
+	val defense: Defense
 ) {
 	companion object {
 		inline fun build(block: Builder.() -> Unit) = Builder().apply(block).build()
@@ -10,15 +11,21 @@ data class Entity(
 
 	class Builder {
 		var health: Double = 0.0
-		var attack: Attack = Attack.build { }
+		private var attack: Attack = Attack.build { }
+		private var defense: Defense = Defense.build { }
 
 		fun attack(block: Attack.Builder.() -> Unit) {
 			attack = Attack.build(block)
 		}
 
+		fun defense(block: Defense.Builder.() -> Unit) {
+			defense = Defense.build(block)
+		}
+
 		fun build() = Entity(
 			health,
-			attack
+			attack,
+			defense
 		)
 	}
 }
