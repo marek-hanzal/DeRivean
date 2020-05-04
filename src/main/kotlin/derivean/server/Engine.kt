a23599c7dc2ca85fbfa258002874727bd1e4bd14
Marek Hanzal <marek.hanzal@x32.cz>
2020-05-04 12:00:34 +0200

    System: Initial server startup

--- src/main/kotlin/derivean/server/Engine.kt
+++ src/main/kotlin/derivean/server/Engine.kt
@@ -1,12 +1,58 @@
 package derivean.server
 
-class Engine {
-	val greeting: String
-		get() {
-			return "Wrooom."
+import com.typesafe.config.ConfigFactory
+import derivean.lib.api.container.IContainer
+import derivean.lib.api.server.IHttpServer
+import derivean.lib.api.upgrade.IUpgradeManager
+import derivean.lib.api.upgrade.IVersionService
+import derivean.lib.container.ContainerFactory
+import derivean.lib.pool.PoolConfig
+import derivean.lib.server.HttpServerConfig
+import derivean.lib.utils.asStamp
+import derivean.server.config.EngineConfig
+import io.github.config4k.extract
+import mu.KotlinLogging
+import kotlin.system.measureTimeMillis
+import kotlin.time.ExperimentalTime
+
+class Engine(container: IContainer) {
+	private val engineConfig: EngineConfig by container.lazy()
+	private val upgradeManager: IUpgradeManager by container.lazy()
+	private val versionService: IVersionService by container.lazy()
+	private val httpServer: IHttpServer by container.lazy()
+	private val logger = KotlinLogging.logger {}
+
+	fun run() {
+		measureTimeMillis {
+			logger.info { "Starting DeRivean Server: version: ${engineConfig.version}, upgrade: [${versionService.getVersion()}]" }
+			versionService.getCollection().toList().apply {
+				logger.info { if (count() > 0) "Installed upgrades:" else "Initial application state" }
+			}.forEach {
+				logger.info { "\t\tstamp: [${it.stamp.asStamp()}], version: [${it.version}]" }
+			}
+			if (upgradeManager.upgrade() > 0) {
+				logger.info { "Installed upgrades:" }
+				versionService.getCollection().forEach {
+					logger.info { "\t\tstamp: [${it.stamp.asStamp()}], version: [${it.version}]" }
+				}
+			}
+		}.also {
+			logger.info { "Boobstrap time ${it}ms" }
 		}
+		httpServer.start("DeRivean Server [${engineConfig.version}]")
+	}
 }
 
-fun main(args: Array<String>) {
-	println(Engine().greeting)
+@ExperimentalTime
+fun main() {
+	ContainerFactory.container().apply {
+		register(EngineConfig::class) { ConfigFactory.load().extract("derivean") }
+		register(PoolConfig::class) { create(EngineConfig::class).pool }
+		register(HttpServerConfig::class) { create(EngineConfig::class).httpServer }
+		configurator(IHttpServer::class) {
+		}
+		configurator(IUpgradeManager::class) {
+		}
+		create(Engine::class).run()
+	}
 }
