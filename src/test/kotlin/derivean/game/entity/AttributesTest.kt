45f613722cebeda42ab9e75b6b3a1b78acb36696
Marek Hanzal <marek.hanzal@x32.cz>
2020-05-11 14:00:27 +0200

    Game: Preparing Attributes as a basic class for holding ... attributes of any Entity

---
+++ src/test/kotlin/derivean/game/entity/AttributesTest.kt
@@ -0,0 +1,36 @@
+package derivean.game.entity
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+internal class AttributesTest {
+	@Test
+	fun `Simple equals`() {
+		val attributes = Attributes(mapOf(
+			"health" to 5.0,
+			"stamina" to 1.0,
+			"mana" to 5.0
+		))
+		assertEquals(attributes, attributes)
+	}
+
+	@Test
+	fun `Simple addition`() {
+		val attributes = Attributes(mapOf(
+			"health" to 25.0,
+			"stamina" to 10.0,
+			"mana" to 5.0
+		))
+		val modifyBy = Attributes(mapOf(
+			"health" to -4.5,
+			"stamina" to -0.5,
+			"mana" to 0.5
+		))
+		attributes += modifyBy
+		assertEquals(Attributes(mapOf(
+			"health" to 20.5,
+			"stamina" to 9.5,
+			"mana" to 5.5
+		)), attributes)
+	}
+}
