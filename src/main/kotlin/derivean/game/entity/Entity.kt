2469feb53784d11c3cebfc382f3cbc48db434a90
Marek Hanzal <marek.hanzal@x32.cz>
2020-09-29 18:10:08 +0200

    Equipment: Preparing Inventory and co.

--- src/main/kotlin/derivean/game/entity/Entity.kt
+++ src/main/kotlin/derivean/game/entity/Entity.kt
@@ -2,14 +2,27 @@ package derivean.game.entity
 
 import derivean.game.attribute.Attribute
 import derivean.game.attribute.Attributes
+import derivean.game.equipment.Inventory
+import derivean.game.equipment.Slot
 
 /**
  * An Entity is responsible for holding Attributes and equipping items.
  */
-data class Entity(val name: String, val attributes: Attributes) {
+data class Entity(
+	val name: String,
+	val attributes: Attributes,
+	val inventory: Inventory,
+) {
 	override fun toString() = name
 
 	fun attributes(vararg attributes: Attribute) = this.attributes.set(*attributes)
+
+	/**
+	 * Add an Equipment to a slot in Inventory (not being Equipped).
+	 */
+	fun slot(equip: Slot) {
+		inventory.slot(equip)
+	}
 
 	companion object {
 		inline fun build(name: String, block: Builder.() -> Unit) = Builder(name).apply(block).build()
@@ -18,14 +31,18 @@ data class Entity(val name: String, val attributes: Attributes) {
 
 	class Builder(val name: String) {
 		private var attributes = Attributes()
+		private var inventory = Inventory()
 
 		fun attributes(vararg values: Attribute) {
 			attributes = Attributes(*values)
 		}
 
+		fun slot(slot: Slot) = inventory.slot(slot)
+
 		fun build() = Entity(
 			name,
 			attributes,
+			inventory,
 		)
 	}
 }
--- src/main/kotlin/derivean/game/mutator/being/HumanMutator.kt
+++ src/main/kotlin/derivean/game/mutator/being/HumanMutator.kt
@@ -1,10 +1,12 @@
-package derivean.game.mutator
+package derivean.game.mutator.being
 
 import derivean.game.attribute.common.health
 import derivean.game.attribute.common.mana
 import derivean.game.attribute.common.physicalDefense
 import derivean.game.attribute.common.strength
 import derivean.game.entity.Entity
+import derivean.game.equipment.equipments.slotHands
+import derivean.game.mutator.AbstractMutator
 
 /**
  * This mutator provides basic attributes and abilities related to human beings.
@@ -17,8 +19,8 @@ class HumanMutator : AbstractMutator() {
 			5.0.strength(),
 			5.0.physicalDefense(),
 		)
-//		entity.equip(
-//
-//		)
+		entity.slot(
+			"hands".slotHands(),
+		)
 	}
 }
