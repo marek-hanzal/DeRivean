06fe362bb7e234d4fc32d1ea2b27b5cfaf4fdaa7
Marek Hanzal <marek.hanzal@x32.cz>
2020-09-29 20:25:05 +0200

    Common: Improved test database

--- src/test/kotlin/derivean/server/CommonTest.kt
+++ src/test/kotlin/derivean/server/CommonTest.kt
@@ -2,7 +2,9 @@ package derivean.server
 
 import com.typesafe.config.ConfigFactory
 import derivean.lib.container.ContainerFactory
+import derivean.lib.container.IContainer
 import derivean.lib.pool.PoolConfig
+import derivean.lib.upgrade.AbstractUpgrade
 import derivean.lib.upgrade.IUpgradeManager
 import derivean.server.config.EngineConfig
 import derivean.server.upgrade.u2020_09_25
@@ -17,6 +19,7 @@ class CommonTest {
 		register(PoolConfig::class) { create(EngineConfig::class).pool }
 		configurator(IUpgradeManager::class) {
 			register(u2020_09_25::class)
+			register(Fixtures::class)
 		}
 		create(IUpgradeManager::class).upgrade()
 	}
@@ -24,5 +27,14 @@ class CommonTest {
 	@Test
 	fun `Entity from database`() {
 		val container = setup()
+	}
+}
+
+class Fixtures(container: IContainer) : AbstractUpgrade(container) {
+	override fun upgrade() {
+		storage.transaction {
+//			Entity.new {
+//			}
+		}
 	}
 }
