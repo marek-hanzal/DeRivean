a70c4660935f93ef4d5729a98c35a5983ad070e9
Marek Hanzal <marek.hanzal@x32.cz>
2020-09-29 16:52:10 +0200

    Common: Removed obsolete pieces from Equipment (will be done in different way); updated readme

--- src/main/kotlin/derivean/game/equipment/AbstractItem.kt
+++
@@ -1,7 +0,0 @@
-package derivean.game.equipment
-
-import derivean.game.attribute.Attributes
-
-abstract class AbstractItem(private val attributes: Attributes) : IItem {
-	override fun attributes() = attributes
-}
