150043c235381af75314fd911a6c3709b5d7ca0f
Marek Hanzal <marek.hanzal@x32.cz>
2020-09-29 17:57:50 +0200

    Equipment: Some initial equipment ideas

--- src/main/kotlin/derivean/game/equipment/IEquipment.kt
+++ src/main/kotlin/derivean/game/equipment/IEquipment.kt
@@ -4,9 +4,4 @@ package derivean.game.equipment
  * Equipment could be... equipped. It could be for example "hands" for
  * Humans or "Armor"... or whatever.
  */
-interface IEquipment {
-	/**
-	 * Name of this equipment.
-	 */
-	val equipment: String
-}
+interface IEquipment
