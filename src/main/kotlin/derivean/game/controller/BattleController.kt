6ba13300fe307086e98c782d2a4b1e1b041bc0ac
Marek Hanzal <marek.hanzal@x32.cz>
2020-09-29 17:02:06 +0200

    Entity: Entities should have name!

---
+++ src/main/kotlin/derivean/game/controller/BattleController.kt
@@ -0,0 +1,9 @@
+package derivean.game.controller
+
+/**
+ * Battle controller is responsible for evaluating all the stuff related to
+ * (automated) battle of two teams.
+ *
+ * Use case of this controller is when a player is just looking on (live) battle.
+ */
+class BattleController : AbstractController()
