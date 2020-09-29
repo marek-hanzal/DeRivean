6f824bc894dafadff121486648d86f80a10e0840
Marek Hanzal <marek.hanzal@x32.cz>
2020-09-29 17:02:06 +0200

    Entity: Entities should have name!

---
+++ src/test/kotlin/derivean/game/entity/EntityTest.kt
@@ -0,0 +1,13 @@
+package derivean.game.entity
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class EntityTest {
+	@Test
+	fun `Entity name`() {
+		val entity = Entity.build("Candle, the Grave Hunter") {
+		}
+		assertEquals("Candle, the Grave Hunter", entity.toString())
+	}
+}
