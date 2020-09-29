852c21a5ef0c236d9242a0b66615c0295971db3f
Marek Hanzal <marek.hanzal@x32.cz>
2020-09-29 20:15:00 +0200

    Common: Preparing basic server side stuff tests

---
+++ src/test/kotlin/derivean/server/CommonTest.kt
@@ -0,0 +1,28 @@
+package derivean.server
+
+import com.typesafe.config.ConfigFactory
+import derivean.lib.container.ContainerFactory
+import derivean.lib.pool.PoolConfig
+import derivean.lib.upgrade.IUpgradeManager
+import derivean.server.config.EngineConfig
+import derivean.server.upgrade.u2020_09_25
+import io.github.config4k.extract
+import org.junit.Test
+import kotlin.time.ExperimentalTime
+
+@ExperimentalTime
+class CommonTest {
+	private fun createContainer() = ContainerFactory.container().apply {
+		register(EngineConfig::class) { ConfigFactory.load().extract("derivean") }
+		register(PoolConfig::class) { create(EngineConfig::class).pool }
+		configurator(IUpgradeManager::class) {
+			register(u2020_09_25::class)
+		}
+	}
+
+	@Test
+	fun `Entity from database`() {
+		val container = createContainer()
+		container.create(IUpgradeManager::class).upgrade()
+	}
+}
