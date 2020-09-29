c2e3aeb1a335531e7bbe6b1b2d3e828cddf99bbf
Marek Hanzal <marek.hanzal@x32.cz>
2020-09-29 17:29:46 +0200

    Controller: Preparing initial entities for battle

--- src/test/kotlin/derivean/game/controller/BattleControllerTest.kt
+++ src/test/kotlin/derivean/game/controller/BattleControllerTest.kt
@@ -1,6 +1,7 @@
 package derivean.game.controller
 
 import derivean.game.entity.Entities
+import derivean.game.entity.Entity
 import derivean.game.mutator.HumanMutator
 import derivean.game.mutator.`class`.WarriorClassMutator
 import org.junit.Test
@@ -8,15 +9,33 @@ import org.junit.Test
 class BattleControllerTest {
 	@Test
 	fun `Do some simple battle, dude!`() {
-		val humanMutator = HumanMutator()
-		val warriorClassMutator = WarriorClassMutator()
 		val controller = BattleController()
 
+		val alfa = createAlfaTeam()
+		val beta = createBetaTeam()
 
-		val alfa = Entities.build {
-			entities(
+		controller.round(alfa, beta)
+	}
 
-			)
+	private fun createAlfaTeam() = Entities.build {
+		val humanMutator = HumanMutator()
+		val warriorClassMutator = WarriorClassMutator()
+
+		with(Entity.build("The Candle Holder")) {
+			humanMutator.mutate(this)
+			warriorClassMutator.mutate(this)
+			entities(this)
+		}
+	}
+
+	private fun createBetaTeam() = Entities.build {
+		val humanMutator = HumanMutator()
+		val warriorClassMutator = WarriorClassMutator()
+
+		with(Entity.build("Wind River")) {
+			humanMutator.mutate(this)
+			warriorClassMutator.mutate(this)
+			entities(this)
 		}
 	}
 }
