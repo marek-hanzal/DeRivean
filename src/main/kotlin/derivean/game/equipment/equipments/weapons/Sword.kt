cd51664678792e6f03315a4e4ac0fd0c08618983
Marek Hanzal <marek.hanzal@x32.cz>
2020-09-29 18:42:10 +0200

    Common: Removed obsolete stuff

--- src/main/kotlin/derivean/game/equipment/equipments/weapons/Sword.kt
+++ src/main/kotlin/derivean/game/equipment/equipments/weapons/Sword.kt
@@ -1,9 +1,6 @@
 package derivean.game.equipment.equipments.weapons
 
-import derivean.game.attribute.Attribute
 import derivean.game.attribute.Attributes
 import derivean.game.equipment.AbstractEquipment
 
 class Sword(attributes: Attributes) : AbstractEquipment(attributes)
-
-fun String.slotSword(vararg attributes: Attribute) = Sword(Attributes(*attributes))
