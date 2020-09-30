06378d4edaa989c88e58827bf72accd93c935974
Marek Hanzal <marek.hanzal@x32.cz>
2020-09-30 18:03:39 +0200

    Common: Another piece of refactor - simplifying Container stuff

--- src/main/kotlin/derivean/lib/container/IContainer.kt
+++ src/main/kotlin/derivean/lib/container/IContainer.kt
@@ -5,6 +5,18 @@ import kotlin.reflect.KClass
 
 interface IContainer {
 	/**
+	 * register a factory to the container
+	 */
+	fun <T> register(factory: IFactory<T>)
+
+	/**
+	 * register a singleton class (service)
+	 */
+	fun <T : Any> register(impl: KClass<T>)
+
+	fun <T : Any, U : T> register(iface: KClass<T>, callback: IContainer.() -> U)
+
+	/**
 	 * register config callback
 	 */
 	fun <T : Any> configurator(iface: KClass<T>, configurator: Configurator)
@@ -13,23 +25,6 @@ interface IContainer {
 	 * configure the given instance if there is any configurator for it
 	 */
 	fun <T : Any> configure(instance: T, configurator: String? = null)
-
-	/**
-	 * register a factory to the container
-	 */
-	fun <T> register(factory: IFactory<T>)
-
-	/**
-	 * register implementation to interface binding
-	 */
-	fun <T : Any, U : T> register(iface: KClass<T>, impl: KClass<U>)
-
-	fun <T : Any, U : T> register(iface: KClass<T>, callback: IContainer.() -> U)
-
-	/**
-	 * register a singleton class (service)
-	 */
-	fun <T : Any> register(impl: KClass<T>)
 
 	/**
 	 * actually creates (returns) an instance; instance type
