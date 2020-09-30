1942e6f81e44290d13ed9ff4fe93ca822eba5505
Marek Hanzal <marek.hanzal@x32.cz>
2020-09-30 20:32:57 +0200

    Common: Equipment does not mean an Entity gets boost (for example it must attack with Fire Sword to get Fire bonus...)

--- src/main/kotlin/derivean/game/equipment/equipments/Hands.kt
+++
@@ -1,6 +0,0 @@
-package derivean.game.equipment.equipments
-
-import derivean.game.attribute.Attributes
-import derivean.game.equipment.AbstractEquipment
-
-class Hands(attributes: Attributes) : AbstractEquipment(attributes)
