b421d32a580b45d8687b1159c880fc1119612159
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-02 22:44:03 +0200

    Game: It's time to try abilities!

--- src/main/kotlin/derivean/game/ability/Abilities.kt
+++ src/main/kotlin/derivean/game/ability/Abilities.kt
@@ -1,8 +1,8 @@
 package derivean.game.ability
 
 class Abilities(private val map: MutableMap<String, IAbility> = mutableMapOf()) {
-	fun ability(ability: Ability) {
-		map[ability.name] = ability
+	fun ability(ability: IAbility) {
+		map[ability.ability] = ability
 	}
 
 	operator fun get(name: String) = map.getOrElse(name) { throw UnknownAbilityException("Requested unknown ability [${name}].") }
