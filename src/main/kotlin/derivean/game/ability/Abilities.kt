6b777896053b8404e80008e03e5cc7ee26ddd289
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-02 22:24:20 +0200

    Game: Mutators looks quite good... but I don't like the name Mutant :| ... like X-Man ...

--- src/main/kotlin/derivean/game/ability/Abilities.kt
+++ src/main/kotlin/derivean/game/ability/Abilities.kt
@@ -4,4 +4,6 @@ class Abilities(private val map: MutableMap<String, IAbility> = mutableMapOf()) 
 	fun ability(ability: Ability) {
 		map[ability.name] = ability
 	}
+
+	operator fun get(name: String) = map.getOrElse(name) { throw UnknownAbilityException("Requested unknown ability [${name}].") }
 }
--- src/main/kotlin/derivean/game/ability/AbstractAbility.kt
+++ src/main/kotlin/derivean/game/ability/AbstractAbility.kt
@@ -1,8 +1,10 @@
 package derivean.game.ability
 
 import derivean.game.attribute.Attributes
+import derivean.game.entity.Entity
 import derivean.game.mutator.IMutator
+import derivean.game.mutator.Mutant
 
 abstract class AbstractAbility(val name: String, val mutator: IMutator, val attributes: Attributes) : IAbility {
-//	override fun use(targets: List<Entity>) = mutator.mutate(entity, targets)
+	override fun use(entity: Entity, targets: List<Entity>) = mutator.mutate(Mutant(entity, attributes), targets)
 }
