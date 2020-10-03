9e025998bf90ac9d538b71e630744f34963c336b
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-03 11:29:41 +0200

    Game: Initiative is working!

--- src/main/kotlin/derivean/game/initiative/Initiative.kt
+++ src/main/kotlin/derivean/game/initiative/Initiative.kt
@@ -2,20 +2,17 @@ package derivean.game.initiative
 
 import derivean.game.attribute.common.initiative
 import derivean.game.entity.Entities
-import derivean.game.entity.Entity
 
 class Initiative : AbstractInitiative() {
-	override fun resolve(entities: List<Entities>): Entity {
-		TODO("Not yet implemented")
+	override fun resolve(entities: List<Entities>) = Entities.build {
+		for (value in entities.filter { it.entities.isNotEmpty() }) {
+			entities(resolve(value))
+		}
+	}.let {
+		resolve(it)
 	}
 
-	override fun resolve(entities: Entities): Entity = entities.entities.maxByOrNull {
+	override fun resolve(entities: Entities) = entities.entities.maxByOrNull {
 		it.value.initiative()
-	}?.value ?: throw NoInitiativeException(
-		if (entities.entities.isEmpty()) {
-			"Entities are empty, cannot resolve initiative."
-		} else {
-			"This is strange, but Initiative cannot resolve an Entity from non-empty Entities."
-		}
-	)
+	}?.value ?: throw NoInitiativeException("Entities are empty, cannot resolve initiative.")
 }
