3c8ad7e1ad9e32d1628fb57953d3dcfcd69b851b
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-06 17:40:30 +0200

    Game: Fixed obsolete constructor

--- src/main/kotlin/derivean/game/initiative/Initiative.kt
+++ src/main/kotlin/derivean/game/initiative/Initiative.kt
@@ -4,9 +4,6 @@ import derivean.game.attribute.common.currentInitiative
 import derivean.game.formation.Formation
 import derivean.game.formation.Formations
 import derivean.game.formation.Member
-import derivean.game.mutator.IMutator
-import derivean.game.mutator.initiative.ResetMutator
-import derivean.game.mutator.initiative.RoundMutator
 
 class Initiative : AbstractInitiative() {
 	override fun resolve(formations: Formations) = formations.map.maxByOrNull { it.value.initiative() }?.value ?: throw NoInitiativeException("Cannot resolve initiative from Formations.")
@@ -18,12 +15,6 @@ class Initiative : AbstractInitiative() {
 	}
 
 	class Builder {
-		var reset: IMutator = ResetMutator()
-		var round: IMutator = RoundMutator()
-
-		fun build() = Initiative(
-			reset,
-			round,
-		)
+		fun build() = Initiative()
 	}
 }
