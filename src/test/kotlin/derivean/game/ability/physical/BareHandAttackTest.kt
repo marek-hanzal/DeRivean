b67bf65863d9003d8f746010b2cf7ea726476cce
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-07 21:35:36 +0200

    Game: Fixed Bare Hand Attack test

--- src/test/kotlin/derivean/game/ability/physical/BareHandAttackTest.kt
+++ src/test/kotlin/derivean/game/ability/physical/BareHandAttackTest.kt
@@ -30,17 +30,18 @@ class BareHandAttackTest {
 		formations["alfa"]["alfa"].targets(BareHandAttack.ABILITY, formations).let {
 			assertEquals(1, it.size)
 			assertEquals(5.0, it.first().rank)
+			it.first().let { target ->
+				target.resolve()
+				assertEquals(5.0, target.entity.damage(), "Source does not contain expected amount of damage.")
+				assertEquals(5.0, target.entity.physicalDamage(), "Source does not contain expected amount of damage.")
+				assertEquals(10.0, target.target.health(), "Target does not have expected amount of health.")
+
+				target.resolve()
+				assertEquals(10.0, target.entity.damage(), "Source does not contain expected amount of damage.")
+				assertEquals(10.0, target.entity.physicalDamage(), "Source does not contain expected amount of damage.")
+				assertEquals(5.0, target.target.health(), "Target does not have expected amount of health.")
+			}
 		}
-
-		formations["alfa"]["alfa"].ability(BareHandAttack.ABILITY, formations["alfa"]["1"])
-		assertEquals(5.0, formations["alfa"]["alfa"].damage(), "Source does not contain expected amount of damage.")
-		assertEquals(5.0, formations["alfa"]["alfa"].physicalDamage(), "Source does not contain expected amount of damage.")
-		assertEquals(10.0, formations["beta"]["beta"].health(), "Target does not have expected amount of health.")
-
-		formations["alfa"]["1"].ability(BareHandAttack.ABILITY, formations["alfa"]["1"])
-		assertEquals(10.0, formations["alfa"]["alfa"].damage(), "Source does not contain expected amount of damage.")
-		assertEquals(10.0, formations["alfa"]["alfa"].physicalDamage(), "Source does not contain expected amount of damage.")
-		assertEquals(5.0, formations["beta"]["beta"].health(), "Target does not have expected amount of health.")
 	}
 
 	@Test
@@ -267,52 +268,59 @@ class BareHandAttackTest {
 			assertEquals(listOf(formations["beta"]["foo-bar"], formations["beta"]["foo"]), it.map { map -> map.target })
 		}
 	}
-//
-//	@Test
-//	fun `Number of targets from an Attribute with same Rank another attempt`() {
-//		val entity = Entity.build("Alfa") {
-//			attributes(
-//				10.0.strength(),
-//			)
-//			ability(
-//				BareHandAttack.ability(
-//					"attack.bare-hands", Attributes(
-//						1.2.bareHandTargets(),
-//					)
-//				)
-//			)
-//		}
-//		val entities = Entities.build {
-//			entity("0") {
-//				attributes(
-//					15.0.health(),
-//					5.0.physicalDefense(),
-//				)
-//			}
-//			entity("1") {
-//				attributes(
-//					15.0.health(),
-//					5.0.physicalDefense(),
-//				)
-//			}
-//			entity("2") {
-//				attributes(
-//					12.0.health(),
-//					5.0.physicalDefense(),
-//				)
-//			}
-//			entity("3") {
-//				attributes(
-//					20.0.health(),
-//				)
-//			}
-//		}
-//
-//		with(entity.targets("attack.bare-hands", entities)) {
-//			assertEquals(2, targets.size, "There are more targets!")
-//			assertEquals(15.0, rank)
-//			assertEquals(listOf(entities["3"], entities["0"]), targets.map { it.entity })
-//			assertEquals(listOf(Target(10.0, entities["3"]), Target(5.0, entities["0"])), targets)
-//		}
-//	}
+
+	@Test
+	fun `Number of targets from an Attribute with same Rank another attempt`() {
+		val formations = Formations.build {
+			formation("alfa") {
+				entity("alfa") {
+					attributes(
+						10.0.strength(),
+					)
+					ability(BareHandAttack.build {
+						attributes(
+							10.0.strength(),
+							1.2.bareHandTargets(),
+						)
+					})
+				}
+				entity("beta") {
+					attributes(
+						15.0.health(),
+					)
+				}
+			}
+			formation("beta") {
+				entity("ooo") {
+					attributes(
+						15.0.health(),
+						5.0.physicalDefense(),
+					)
+				}
+				entity("foo") {
+					attributes(
+						15.0.health(),
+						5.0.physicalDefense(),
+					)
+				}
+				entity("bar") {
+					attributes(
+						12.0.health(),
+						5.0.physicalDefense(),
+					)
+				}
+				entity("foo-bar") {
+					attributes(
+						20.0.health(),
+					)
+				}
+			}
+		}
+
+		formations["alfa"]["alfa"].targets(BareHandAttack.ABILITY, formations).let {
+			assertEquals(2, it.size, "There are more targets!")
+			assertEquals(35.0, it.sumByDouble { sum -> sum.rank })
+			assertEquals(listOf(formations["beta"]["foo-bar"], formations["beta"]["ooo"]), it.map { map -> map.target })
+		}
+	}
 }
