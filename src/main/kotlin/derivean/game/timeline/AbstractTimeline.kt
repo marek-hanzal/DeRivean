3bd97679901d1384049fcdcbdbedf7d1ae713f20
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-10 23:24:40 +0200

    Game: Working on Timeline

--- src/main/kotlin/derivean/game/timeline/AbstractTimeline.kt
+++ src/main/kotlin/derivean/game/timeline/AbstractTimeline.kt
@@ -1,3 +1,7 @@
 package derivean.game.timeline
 
-abstract class AbstractTimeline(val time: Double) : ITimeline
+abstract class AbstractTimeline(var time: Double, val step: Double, val entries: MutableList<Entry>) : ITimeline {
+	fun step() {
+		time += step
+	}
+}
--- src/main/kotlin/derivean/game/timeline/Timeline.kt
+++ src/main/kotlin/derivean/game/timeline/Timeline.kt
@@ -1,9 +1,11 @@
 package derivean.game.timeline
 
-class Timeline(time: Double) : AbstractTimeline(time) {
+class Timeline(time: Double, step: Double, entries: MutableList<Entry>) : AbstractTimeline(time, step, entries) {
 	fun entry(block: Entry.Builder.() -> Unit) = Entry.build(block)
 
 	override fun loop() {
+
+		step()
 		TODO("Not yet implemented")
 	}
 
@@ -12,10 +14,20 @@ class Timeline(time: Double) : AbstractTimeline(time) {
 	}
 
 	class Builder {
+		/**
+		 * Current time on the Timeline.
+		 */
 		var time = 0.0
+
+		/**
+		 * Time increase per loop (usually not needed to change).
+		 */
+		var step = 1.0
 
 		fun build() = Timeline(
 			time,
+			step,
+			mutableListOf(),
 		)
 	}
 }
