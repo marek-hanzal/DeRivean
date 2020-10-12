d956d4097a46fec6981ae209215cbb164d834dfb
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-12 17:35:57 +0200

    Server: Experimenting with logger setup

--- src/main/kotlin/derivean/server/EngineContainer.kt
+++ src/main/kotlin/derivean/server/EngineContainer.kt
@@ -10,6 +10,7 @@ import derivean.lib.upgrade.IUpgradeManager
 import derivean.server.config.EngineConfig
 import derivean.server.entity.EntityRepository
 import derivean.server.http.ClientModule
+import derivean.server.http.DiscoveryHttpModule
 import derivean.server.player.PlayerRepository
 import derivean.server.upgrade.u2020_09_25
 import derivean.server.upgrade.u2020_10_12
@@ -20,9 +21,19 @@ object EngineContainer {
 		register(EngineConfig::class) { ConfigFactory.load().extract("derivean") }
 		register(PoolConfig::class) { create(EngineConfig::class).pool }
 		register(HttpServerConfig::class) { create(EngineConfig::class).httpServer }
+		/**
+		 * Upgrades
+		 */
 		register(u2020_09_25::class) { u2020_09_25(this) }
 		register(u2020_10_12::class) { u2020_10_12(this) }
+		/**
+		 * Http Modules
+		 */
+		register(DiscoveryHttpModule::class) { DiscoveryHttpModule(this) }
 		register(ClientModule::class) { ClientModule(this) }
+		/**
+		 * Server-side Services
+		 */
 		service(PlayerRepository::class) { PlayerRepository(this) }
 		service(EntityRepository::class) { EntityRepository(this) }
 		configurator(IUpgradeManager::class) {
---
+++ src/main/kotlin/derivean/server/http/DiscoveryHttpModule.kt
@@ -0,0 +1,20 @@
+package derivean.server.http
+
+import derivean.lib.container.IContainer
+import derivean.lib.rest.discovery
+import derivean.lib.rest.discovery.IDiscoveryService
+import derivean.lib.server.AbstractHttpModule
+import io.ktor.application.*
+import io.ktor.routing.*
+
+class DiscoveryHttpModule(container: IContainer) : AbstractHttpModule(container) {
+	private val discoveryService: IDiscoveryService by container.lazy()
+
+	override fun install(routing: Routing) {
+		routing {
+			get("/discovery") {
+				call.discovery(discoveryService.discovery())
+			}
+		}
+	}
+}
