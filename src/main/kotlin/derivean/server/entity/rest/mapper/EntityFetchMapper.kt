62edef71fab84e61da22034191f5d5e4386d0ab6
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-12 23:00:40 +0200

    Server: Simplified mappers

--- src/main/kotlin/derivean/server/entity/rest/mapper/EntityFetchMapper.kt
+++ src/main/kotlin/derivean/server/entity/rest/mapper/EntityFetchMapper.kt
@@ -3,10 +3,31 @@ package derivean.server.entity.rest.mapper
 import derivean.lib.container.IContainer
 import derivean.lib.mapper.AbstractMapper
 import derivean.server.entity.Entity
+import org.jetbrains.exposed.dao.EntityID
+import java.util.*
 
-class EntityFetchMapper(container: IContainer) : AbstractMapper<Entity, Fetch>(container) {
+class EntityFetchMapper(container: IContainer) : AbstractMapper<Entity, EntityFetchMapper.Fetch>(container) {
 	override fun map(item: Entity) = Fetch.build {
 		this.id = item.id
 		this.name = item.name
+	}
+
+	data class Fetch(
+		val id: String,
+		val name: String,
+	) {
+		companion object {
+			inline fun build(block: Builder.() -> Unit) = Builder().apply(block).build()
+		}
+
+		class Builder {
+			lateinit var id: EntityID<UUID>
+			lateinit var name: String
+
+			fun build() = Fetch(
+				id.toString(),
+				name,
+			)
+		}
 	}
 }
--- src/main/kotlin/derivean/server/player/rest/mapper/PlayerFetchMapper.kt
+++ src/main/kotlin/derivean/server/player/rest/mapper/PlayerFetchMapper.kt
@@ -3,10 +3,31 @@ package derivean.server.player.rest.mapper
 import derivean.lib.container.IContainer
 import derivean.lib.mapper.AbstractMapper
 import derivean.server.player.Player
+import org.jetbrains.exposed.dao.EntityID
+import java.util.*
 
-class PlayerFetchMapper(container: IContainer) : AbstractMapper<Player, Fetch>(container) {
+class PlayerFetchMapper(container: IContainer) : AbstractMapper<Player, PlayerFetchMapper.Fetch>(container) {
 	override fun map(item: Player) = Fetch.build {
 		this.id = item.id
 		this.name = item.name
+	}
+
+	data class Fetch(
+		val id: String,
+		val name: String,
+	) {
+		companion object {
+			inline fun build(block: Builder.() -> Unit) = Builder().apply(block).build()
+		}
+
+		class Builder {
+			lateinit var id: EntityID<UUID>
+			lateinit var name: String
+
+			fun build() = Fetch(
+				id.toString(),
+				name,
+			)
+		}
 	}
 }
