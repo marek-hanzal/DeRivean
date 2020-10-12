5cf742385beae659ff5f731ebbeef4959dce8688
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-12 23:24:02 +0200

    Server: Fixed Player pages

--- src/main/kotlin/derivean/server/player/rest/PlayerPagesEndpoint.kt
+++ src/main/kotlin/derivean/server/player/rest/PlayerPagesEndpoint.kt
@@ -3,21 +3,14 @@ package derivean.server.player.rest
 import derivean.lib.container.IContainer
 import derivean.lib.rest.page.AbstractPagesEndpoint
 import derivean.server.player.PlayerRepository
-import io.ktor.application.*
 import io.ktor.routing.*
 
 class PlayerPagesEndpoint(container: IContainer) : AbstractPagesEndpoint(container) {
 	private val playerRepository: PlayerRepository by container.lazy()
 
-	override fun install(routing: Routing) {
-		discovery {
-			group = "player"
-			name = "pages"
-			link = "/player/pages"
-			description = "Access to available pages of players (just prepared paging)."
-		}
-		routing.get("/player/pages") {
-			pageService.pages(call, "/player/page/{page}", playerRepository)
-		}
-	}
+	override fun install(routing: Routing) = pages(
+		routing,
+		"player",
+		playerRepository,
+	)
 }
