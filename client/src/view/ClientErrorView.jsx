4e3c4e83c083b9751ba7ff2dc46f8a6da51f16e8
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-13 14:57:20 +0200

    Client: Added title

--- client/src/view/ClientErrorView.jsx
+++ client/src/view/ClientErrorView.jsx
@@ -1,14 +1,18 @@
 import {Button, Result} from 'antd';
 import React from 'react';
+import {Helmet} from 'react-helmet';
 import {withTranslation} from 'react-i18next';
 
 const ClientErrorView = ({t}) =>
-	<Result
-		status="500"
-		title={t('error.client.title')}
-		subTitle={t('error.client.body')}
-		extra={<Button type="primary" onClick={() => window.location.reload()}>{t('common.refresh')}</Button>}
-	/>
+	<>
+		<Helmet title={t('error.client.title')}/>
+		<Result
+			status="500"
+			title={t('error.client.title')}
+			subTitle={t('error.client.body')}
+			extra={<Button type="primary" onClick={() => window.location.reload()}>{t('common.refresh')}</Button>}
+		/>
+	</>
 ;
 
 export default withTranslation()(ClientErrorView);
--- client/src/view/DiscoveryErrorView.jsx
+++ client/src/view/DiscoveryErrorView.jsx
@@ -1,14 +1,18 @@
 import {Button, Result} from 'antd';
 import React from 'react';
+import {Helmet} from 'react-helmet';
 import {withTranslation} from 'react-i18next';
 
 const DiscoveryErrorView = ({t}) =>
-	<Result
-		status="500"
-		title={t('error.discovery.title')}
-		subTitle={t('error.discovery.body')}
-		extra={<Button type="primary" onClick={() => window.location.reload()}>{t('common.refresh')}</Button>}
-	/>
+	<>
+		<Helmet title={t('error.discovery.title')}/>
+		<Result
+			status="500"
+			title={t('error.discovery.title')}
+			subTitle={t('error.discovery.body')}
+			extra={<Button type="primary" onClick={() => window.location.reload()}>{t('common.refresh')}</Button>}
+		/>
+	</>
 ;
 
 export default withTranslation()(DiscoveryErrorView);
