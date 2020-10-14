124262135ddad76c858e96c71616870fb465483a
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-14 19:51:31 +0200

    Server: Disabled intention

--- src/main/kotlin/derivean/lib/mapper/AbstractMapper.kt
+++ src/main/kotlin/derivean/lib/mapper/AbstractMapper.kt
@@ -4,5 +4,6 @@ import derivean.lib.container.AbstractService
 import derivean.lib.container.IContainer
 
 abstract class AbstractMapper<T, U>(container: IContainer) : AbstractService(container), IMapper<T, U> {
+	@Suppress("UNCHECKED_CAST")
 	fun toMapper() = this as IMapper<Any, out Any>
 }
