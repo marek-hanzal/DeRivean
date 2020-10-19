45a7e078db00016cf075b9221747faa26f477fe4
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-19 18:18:30 +0200

    Client: Improved registration workflow

--- client/src/site/public/view/registration/RegistrationSucceed.jsx
+++ client/src/site/public/view/registration/RegistrationSucceed.jsx
@@ -1,31 +1,23 @@
 import {Button, Result} from "antd";
 import React from "react";
 import {withTranslation} from "react-i18next";
-import {connect} from "react-redux";
 import {Link} from "react-router-dom";
-import {onSessionOpen} from "redux/session/action";
-import {getUserRegisterUser} from "redux/user/register/selector";
+import PublicPath from "site/public/router/PublicPath";
 
 const RegistrationSucceed = (
 	{
 		t,
-		onSubmit,
 	}) =>
 	<Result
 		status="success"
 		title={t("public.registration.succeed.title")}
 		subTitle={t("public.registration.succeed.subtitle")}
 		extra={[
-			<Button type="primary" key="close" onClick={() => onSubmit()}>
-				<Link to={"/"}>{t("public.registration.continue.title")}</Link>
+			<Button type="primary" key="continue">
+				<Link to={PublicPath.login}>{t("public.registration.continue.title")}</Link>
 			</Button>
 		]}
 	/>
 ;
 
-export default connect(
-	state => ({}),
-	{
-		onSubmit: () => (dispatch, getState) => dispatch(onSessionOpen(getUserRegisterUser(getState()))),
-	},
-)(withTranslation()(RegistrationSucceed));
+export default withTranslation()(RegistrationSucceed);
