6e5d91400edb8f8bf5e3227de6c77fed2cc839e0
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-20 10:55:11 +0200

    Client: Fixed cached loading language files

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -1,16 +1,21 @@
-import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import Backend from 'i18next-xhr-backend';
-import {initReactI18next} from 'react-i18next';
+import buildUrl from "build-url";
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-xhr-backend";
+import {initReactI18next} from "react-i18next";
 
 i18n
 	.use(initReactI18next)
 	.use(Backend)
 	.use(LanguageDetector)
 	.init({
-		fallbackLng: 'en',
+		fallbackLng: "en",
 		backend: {
-			loadPath: '/locale/{{lng}}.json',
+			loadPath: buildUrl("/locale/{{lng}}.json", {
+				queryParams: {
+					version: process.env.REACT_APP_VERSION,
+				}
+			}),
 		},
 		interpolation: {
 			escapeValue: false,
