a484e63481f37f88e5bf502ba8ac5dfe3f5f31e8
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-20 11:06:50 +0200

    Server: Fixed signup view - it's nice and shiny now!

--- client/src/site/public/view/sign-up/SignUpView.jsx
+++ client/src/site/public/view/sign-up/SignUpView.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import {withTranslation} from "react-i18next";
 import {connect} from "react-redux";
 import {getUserRegisterStatus} from "redux/user/register/selector";
+import MinimalView from "site/public/component/MinimalView";
 import PublicView from "site/public/component/PublicView";
 import PublicPath from "site/public/router/PublicPath";
 import FailedResult from "site/public/view/sign-up/FailedResult";
@@ -12,18 +13,33 @@ import SucceedResult from "site/public/view/sign-up/SucceedResult";
 function resolveStatus(t, status) {
 	switch (status) {
 		case "SUCCESS":
-			return <SucceedResult/>;
+			return (
+				<MinimalView>
+					<SucceedResult/>
+				</MinimalView>
+			);
 		case "FAILURE":
-			return <FailedResult/>;
+			return (
+				<MinimalView>
+					<FailedResult/>
+				</MinimalView>
+			);
 		default:
 			return (
-				<Row justify={"center"}>
-					<Col span={8}>
-						<Card title={t("public.sign-up.title")}>
-							<SignUpForm/>
-						</Card>
-					</Col>
-				</Row>
+				<PublicView
+					title={"public.sign-up.title"}
+					subtitle={"public.sign-up.subtitle"}
+					open={[PublicPath.root]}
+					selected={[PublicPath.signUp]}
+				>
+					<Row justify={"center"}>
+						<Col span={8}>
+							<Card title={t("public.sign-up.title")}>
+								<SignUpForm/>
+							</Card>
+						</Col>
+					</Row>
+				</PublicView>
 			);
 	}
 }
@@ -32,14 +48,7 @@ const SignUpView = (
 	{
 		t,
 		status,
-	}) =>
-	<PublicView
-		title={"public.sign-up.title"}
-		subtitle={"public.sign-up.subtitle"}
-		open={[PublicPath.root]}
-		selected={[PublicPath.signUp]}
-		children={resolveStatus(t, status)}
-	/>
+	}) => resolveStatus(t, status)
 ;
 
 export default connect(
