488a88b37945c93cc550121d1d0db3591d8eebfb
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-21 22:40:58 +0200

    Client: Fixed user session

--- client/src/redux/session/action.js
+++ client/src/redux/session/action.js
@@ -1,7 +1,7 @@
 import {createAction} from "redux-actions";
 
 const
-	onSessionOpen = createAction("ON_SESSION_OPEN", user => user),
+	onSessionOpen = createAction("ON_SESSION_OPEN", user => ({user})),
 	onSessionClose = createAction("ON_SESSION_CLOSE", () => ({user: {site: "public"}}));
 
 export {
