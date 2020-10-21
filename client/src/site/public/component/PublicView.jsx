a97172cd6c3efc3df8e126f3ebb2f0f089567c9a
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-21 17:05:26 +0200

    Client: Removed main menu component of public

--- client/src/site/public/component/PublicView.jsx
+++ client/src/site/public/component/PublicView.jsx
@@ -1,9 +1,13 @@
+import {FormOutlined, HomeOutlined, LoginOutlined} from "@ant-design/icons";
 import Layout from "component/Layout";
+import Menu from "component/Menu";
 import React from "react";
 import Footer from "site/public/component/Footer";
 import Header from "site/public/component/Header";
-import MainMenu from "site/public/component/MainMenu";
 import PublicBreadcrumbs from "site/public/component/PublicBreadcrumbs";
+import PublicPath from "site/public/router/PublicPath";
+import menu from "utils/menu/menu";
+import menuItem from "utils/menu/menuItem";
 
 const PublicView = (
 	{
@@ -16,9 +20,16 @@ const PublicView = (
 	<Layout
 		title={title}
 		subtitle={subtitle}
-		menu={<MainMenu
+		menu={<Menu
 			open={open}
 			selected={selected}
+			items={[
+				menu(PublicPath.root, [
+					menuItem(PublicPath.root, "public.home", <HomeOutlined/>),
+					menuItem(PublicPath.signUp, "public.sign-up", <FormOutlined/>),
+					menuItem(PublicPath.signIn, "public.sign-in", <LoginOutlined/>),
+				])
+			]}
 		/>}
 		breadcrumbs={<PublicBreadcrumbs/>}
 		header={<Header/>}
