a3e8238e4f895be0072d3a00a331b88f87aefc0a
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-23 14:34:00 +0200

    Client: Nice sign-out view

--- client/src/component/view/SingOutView.jsx
+++ client/src/component/view/SingOutView.jsx
@@ -1,14 +1,11 @@
-import { QuestionCircleFilled } from "@ant-design/icons";
-import {
-	Button,
-	Card,
-	Result
-} from "antd";
+import {QuestionCircleFilled} from "@ant-design/icons";
+import {Button, Card, Result} from "antd";
+import WithMenu from "component/view/WithMenu";
 import React from "react";
-import { withTranslation } from "react-i18next";
-import { connect } from "react-redux";
-import { withRouter } from "react-router";
-import { onSessionClose } from "redux/session/action";
+import {withTranslation} from "react-i18next";
+import {connect} from "react-redux";
+import {withRouter} from "react-router";
+import {onSessionClose} from "redux/session/action";
 
 const SingOutView = (
 	{
@@ -16,21 +13,24 @@ const SingOutView = (
 		translation,
 		history,
 		onLogout,
+		menu,
 	}) =>
-	<Card>
-		<Result
-			icon={<QuestionCircleFilled/>}
-			title={t(`${translation}.sign-out.title`)}
-			subTitle={t(`${translation}.sign-out.subtitle`)}
-			extra={[
-				<Button type="primary" key="sign-out" onClick={() => onLogout()}>
-					{t(`${translation}.sign-out.button.sign-out`)}
-				</Button>,
-				<Button key="back" onClick={() => history.goBack()}>{t(`${translation}.sign-out.button.back`)}</Button>,
-			]}
-		>
-		</Result>
-	</Card>
+	<WithMenu men={menu}>
+		<Card>
+			<Result
+				icon={<QuestionCircleFilled/>}
+				title={t(`${translation}.sign-out.title`)}
+				subTitle={t(`${translation}.sign-out.subtitle`)}
+				extra={[
+					<Button type="primary" key="sign-out" onClick={() => onLogout()}>
+						{t(`${translation}.sign-out.button.sign-out`)}
+					</Button>,
+					<Button key="back" onClick={() => history.goBack()}>{t(`${translation}.sign-out.button.back`)}</Button>,
+				]}
+			>
+			</Result>
+		</Card>
+	</WithMenu>
 ;
 
 export default connect(
