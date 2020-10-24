61b86b19ab8cf7a9e46f94f370446b904c55accb
Marek Hanzal <marek.hanzal@x32.cz>
2020-10-24 15:57:17 +0200

    Client: User breadcrumbs works

--- client/src/site/root/module/user/site/UserBreadcrumbs.jsx
+++ client/src/site/root/module/user/site/UserBreadcrumbs.jsx
@@ -1,4 +1,5 @@
 import BaseBreadcrumbs from "component/breadcrumbs/BaseBreadcrumbs";
+import CreateIcon from "component/icon/CreateIcon";
 import HomeIcon from "component/icon/HomeIcon";
 import {Route, Routes} from "react-router";
 import UserDashboardIcon from "site/root/module/user/component/icon/UserDashboardIcon";
@@ -6,6 +7,7 @@ import UserPath from "site/root/module/user/site/UserPath";
 import RootPath from "site/root/site/RootPath";
 import breadcrumbCurrentItem from "utils/breadcrumbs/breadcrumbCurrentItem";
 import breadcrumbIconItem from "utils/breadcrumbs/breadcrumbIconItem";
+import breadcrumbItem from "utils/breadcrumbs/breadcrumbItem";
 
 const DashboardBreadcrumbs = () =>
 	<BaseBreadcrumbs
@@ -16,10 +18,20 @@ const DashboardBreadcrumbs = () =>
 	/>
 ;
 
+const CreateBreadcrumbs = () =>
+	<BaseBreadcrumbs
+		items={[
+			breadcrumbIconItem(RootPath.root, <HomeIcon/>),
+			breadcrumbItem("../" + UserPath.dashboard, "root.user.dashboard", <UserDashboardIcon/>),
+			breadcrumbCurrentItem("root.user.create", <CreateIcon/>),
+		]}
+	/>
+;
+
 const UserBreadcrumbs = () =>
 	<Routes>
 		<Route path={UserPath.dashboard} element={<DashboardBreadcrumbs/>}/>
-		<Route path={UserPath.create} element={<DashboardBreadcrumbs/>}/>
+		<Route path={UserPath.create} element={<CreateBreadcrumbs/>}/>
 	</Routes>
 ;
 
