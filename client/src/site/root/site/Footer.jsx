159b21f10d4471d56bfe953624d11a0c97d73546
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-06 23:38:59 +0100

    Client: Trying to add git version to the env vars

--- client/src/site/root/site/Footer.jsx
+++ client/src/site/root/site/Footer.jsx
@@ -2,7 +2,7 @@ import {Layout} from "antd";
 
 const Footer = () =>
 	<Layout.Footer style={{textAlign: "center"}}>
-		v[{process.env.REACT_APP_VERSION}]
+		v[{process.env.REACT_APP_VERSION}@{process.env.REACT_APP_HASH}]
 	</Layout.Footer>
 ;
 
