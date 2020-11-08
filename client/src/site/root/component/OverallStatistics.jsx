311879bc6bc321f8450dd1f267ec910be1647aea
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-08 19:34:02 +0100

    Client: Handled 401 for statistics

--- client/src/site/root/component/OverallStatistics.jsx
+++ client/src/site/root/component/OverallStatistics.jsx
@@ -1,4 +1,4 @@
-import {Card, Divider, message, Space, Spin, Statistic} from "antd";
+import {Card, Divider, message, Space, Spin, Statistic, Tooltip} from "antd";
 import {useState} from "react";
 import {useTranslation} from "react-i18next";
 import {useStatistics} from "redux/statistics/redux";
@@ -12,6 +12,7 @@ const OverallStatistics = (
 	}) => {
 	const {t} = useTranslation();
 	const [data, setData] = useState({users: 0, kingdoms: 0, heroes: 0, buildings: 0});
+	const [unavailable, setUnavailable] = useState(false);
 	const [loading, setLoading] = useState(true);
 	action = action || useStatistics;
 	action(
@@ -22,15 +23,29 @@ const OverallStatistics = (
 		() => {
 			setLoading(false);
 			message.error(t("root.statistic.fetch-error"));
+		},
+		{
+			401: () => {
+				setUnavailable(true);
+			}
 		}
 	);
 	return (
 		<Card style={{textAlign: "center"}}>
-			<Spin spinning={loading}>
-				<Space size={"large"} split={<Divider type={"vertical"}/>} style={{textAlign: "center"}}>
-					{show.filter(value => !exclude.includes(value)).map(item => <Statistic key={item} title={t(`root.statistic.${item}`)} value={data[item]} prefix={<ModuleIcon module={item}/>}/>)}
-				</Space>
-			</Spin>
+			{unavailable ?
+				<Tooltip defaultVisible title={t("root.statistics.unavailable")}>
+					<Spin spinning={true} indicator={null}>
+						<Space size={"large"} split={<Divider type={"vertical"}/>} style={{textAlign: "center"}}>
+							{show.filter(value => !exclude.includes(value)).map(item => <Statistic key={item} title={t(`root.statistic.${item}`)} value={data[item]} prefix={<ModuleIcon module={item}/>}/>)}
+						</Space>
+					</Spin>
+				</Tooltip> :
+				<Spin spinning={loading}>
+					<Space size={"large"} split={<Divider type={"vertical"}/>} style={{textAlign: "center"}}>
+						{show.filter(value => !exclude.includes(value)).map(item => <Statistic key={item} title={t(`root.statistic.${item}`)} value={data[item]} prefix={<ModuleIcon module={item}/>}/>)}
+					</Space>
+				</Spin>
+			}
 		</Card>
 	);
 };
