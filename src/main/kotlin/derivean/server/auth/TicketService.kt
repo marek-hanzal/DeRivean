11d10e23ed2baf2ad229b954fc8a0643aed27cdf
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-08 01:31:51 +0100

    Server: Just for test CIB

---
+++ src/main/kotlin/derivean/server/auth/TicketService.kt
@@ -0,0 +1,6 @@
+package derivean.server.auth
+
+import derivean.lib.container.AbstractService
+import derivean.lib.container.IContainer
+
+class TicketService(container: IContainer) : AbstractService(container)
