5a2c3067bfe1a58599d2d567fb4eadb241b7a61b
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-08 17:08:53 +0100

    Client: Removed first Redux class, yaaaay!

--- client/src/redux/server/redux.js
+++ client/src/redux/server/redux.js
@@ -1,24 +1,17 @@
 import axios from "axios";
 import {useEffect} from "react";
-import {useDispatch, useStore} from "react-redux";
+import {useStore} from "react-redux";
 import {selectLink} from "redux/discovery/redux";
-import {Server} from "server";
-import CreateLinkRedux from "utils/redux/CreateLinkRedux";
-import CreateRedux from "utils/redux/CreateRedux";
-
-const ServerRedux = CreateRedux({
-	validate: CreateLinkRedux("server", "validate", "root.server.validate"),
-	sites: CreateLinkRedux("server", "sites", "root.server.sites"),
-});
+import get from "utils/server/get";
 
 const useServerSites = (
 	onSuccess = sites => null,
 	onError = error => null,
 ) => {
-	const dispatch = useDispatch();
-	const cancelToken = axios.CancelToken.source();
+	const store = useStore();
 	useEffect(() => {
-		dispatch(ServerRedux.redux.sites.dispatch.sites(cancelToken)).then(onSuccess, onError);
+		const cancelToken = axios.CancelToken.source();
+		get(selectLink("root.server.sites", store.getState()), onSuccess, onError, cancelToken);
 		return () => cancelToken.cancel();
 		// eslint-disable-next-line
 	}, [dispatch]);
@@ -31,25 +24,13 @@ const useServerValidate = (
 	const store = useStore();
 	useEffect(() => {
 		const cancelToken = axios.CancelToken.source();
-		Server.get(selectLink("root.server.validate", store.getState()), {
-			cancelToken: cancelToken.token,
-		})
-			.then(({data}) => {
-				onSuccess(data);
-			})
-			.catch(error => {
-				if (axios.isCancel(error)) {
-					return Promise.reject({cancel: true});
-				}
-				onError(error);
-			});
+		get(selectLink("root.server.validate", store.getState()), onSuccess, onError, cancelToken);
 		return () => cancelToken.cancel();
 		// eslint-disable-next-line
 	}, [store]);
 };
 
 export {
-	ServerRedux,
 	useServerSites,
 	useServerValidate,
 };
