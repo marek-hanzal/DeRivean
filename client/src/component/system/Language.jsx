f668bc4610830729519448f5d13aafc32bc44d1b
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-08 22:24:48 +0100

    Client: Removed create link redux, yaaay!

--- client/src/component/system/Language.jsx
+++ client/src/component/system/Language.jsx
@@ -1,25 +1,19 @@
 import i18next from "i18next";
-import {useEffect, useState} from "react";
-import {useDispatch} from "react-redux";
-import {TranslationRedux} from "redux/translation/redux";
+import {useState} from "react";
+import {useTranslation} from "redux/translation/redux";
 import LanguageErrorView from "view/LanguageErrorView";
 import LoaderView from "view/LoaderView";
 
 const Language = ({children}) => {
 	const [status, setStatus] = useState();
-	const dispatch = useDispatch();
-	useEffect(() => {
-		dispatch(TranslationRedux.redux.translation.dispatch.translation()).then(({translations}) => {
-			for (const translation of translations) {
-				i18next.addResource(translation.language, translation.namespace, translation.label, translation.text);
-			}
-			setStatus(true);
-		}, () => {
-			setStatus(false);
-		});
-		// eslint-disable-next-line
-	}, [dispatch]);
-
+	useTranslation(({translations}) => {
+		for (const translation of translations) {
+			i18next.addResource(translation.language, translation.namespace, translation.label, translation.text);
+		}
+		setStatus(true);
+	}, () => {
+		setStatus(false);
+	});
 	switch (status) {
 		case true:
 			return children;
--- client/src/redux/translation/redux.js
+++ client/src/redux/translation/redux.js
@@ -1,23 +1,47 @@
+import axios from "axios";
+import {useEffect} from "react";
+import {useStore} from "react-redux";
+import {useNavigate} from "react-router";
+import {selectLink} from "redux/discovery/redux";
 import commonFetchHook from "utils/hook/commonFetchHook";
 import CreateCommonRedux from "utils/redux/CreateCommonRedux";
-import CreateLinkRedux from "utils/redux/CreateLinkRedux";
 import fetchPage from "utils/server/fetchPage";
+import get from "utils/server/get";
+import resolveReason from "utils/server/resolveReason";
 
 const TranslationRedux = CreateCommonRedux(
 	"translation",
 	"root.translation.create",
 	"root.translation.update",
 	"root.translation.delete",
-	{
-		translation: CreateLinkRedux("translation", "translation", "public.translation"),
-	}
 );
 
+const useTranslation = (
+	onSuccess = validation => null,
+	onError = error => null,
+	onReason = null,
+) => {
+	const store = useStore();
+	const navigate = useNavigate();
+	useEffect(() => {
+		const cancelToken = axios.CancelToken.source();
+		get(
+			selectLink("public.translation", store.getState()),
+			onSuccess,
+			onError,
+			cancelToken,
+			resolveReason(onReason, navigate),
+		);
+		return () => cancelToken.cancel();
+		// eslint-disable-next-line
+	}, [store]);
+};
 const useTranslationFetch = commonFetchHook("root.translation.fetch");
 const fetchTranslationPage = fetchPage("root.translation.page");
 
 export {
 	TranslationRedux,
+	useTranslation,
 	useTranslationFetch,
 	fetchTranslationPage,
 };
