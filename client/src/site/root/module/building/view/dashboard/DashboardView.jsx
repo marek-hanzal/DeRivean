0946da1af5e0166a9ce5809e73c6c03a2accfa97
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-09 22:19:30 +0100

    Client: Fixed some bugs; building are not redirecting after deletion

--- client/src/site/root/module/building/view/dashboard/DashboardView.jsx
+++ client/src/site/root/module/building/view/dashboard/DashboardView.jsx
@@ -1,18 +1,24 @@
 import BaseDashboardView from "component/view/BaseDashboardView";
+import {useParams} from "react-router";
 import {useKingdomStatisticsFetch} from "redux/kingdom/redux";
 import BuildingContext from "site/root/module/building/component/BuildingContext";
 import BuildingStatistics from "site/root/module/building/component/BuildingStatistics";
 import BuildingView from "site/root/module/building/view/BuildingView";
 
 const DashboardView = () => {
+	const params = useParams();
 	return (
 		<BuildingView>
 			<BaseDashboardView
 				context={BuildingContext}
 				menu={"root.building.dashboard"}
-			>
-				<BuildingStatistics action={useKingdomStatisticsFetch}/>
-			</BaseDashboardView>
+				children={
+					<BuildingStatistics show={["buildings"]} action={(onSuccess, onFailure, onReason) => {
+						// eslint-disable-next-line
+						useKingdomStatisticsFetch(params.kingdom, onSuccess, onFailure, onReason);
+					}}/>
+				}
+			/>
 		</BuildingView>
 	);
 };
--- client/src/site/root/module/hero/view/dashboard/DashboardView.jsx
+++ client/src/site/root/module/hero/view/dashboard/DashboardView.jsx
@@ -1,15 +1,22 @@
 import BaseDashboardView from "component/view/BaseDashboardView";
+import {useParams} from "react-router";
 import {useKingdomStatisticsFetch} from "redux/kingdom/redux";
 import HeroContext from "site/root/module/hero/component/HeroContext";
 import HeroStatistics from "site/root/module/hero/component/HeroStatistics";
 import HeroView from "site/root/module/hero/view/HeroView";
 
 const DashboardView = () => {
+	const params = useParams();
 	return (
 		<HeroView>
-			<BaseDashboardView context={HeroContext}>
-				<HeroStatistics action={useKingdomStatisticsFetch}/>
-			</BaseDashboardView>
+			<BaseDashboardView
+				context={HeroContext}
+				children={
+					<HeroStatistics show={["heroes"]} action={(onSuccess, onFailure, onReason) => {
+						// eslint-disable-next-line
+						useKingdomStatisticsFetch(params.kingdom, onSuccess, onFailure, onReason);
+					}}/>
+				}/>
 		</HeroView>
 	);
 };
