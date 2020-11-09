cce48bb515b3bb8b4e41c0ac5ae210afbbfa6800
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-09 11:25:00 +0100

    Client: Removed one usage of PostRedux

--- client/src/redux/server/redux.js
+++ client/src/redux/server/redux.js
@@ -1,19 +1,20 @@
 import axios from "axios";
-import {useEffect} from "react";
-import {useStore} from "react-redux";
-import {useNavigate} from "react-router";
-import {selectLink} from "redux/discovery/redux";
+import { useEffect } from "react";
+import { useStore } from "react-redux";
+import { useNavigate } from "react-router";
+import { selectLink } from "redux/discovery/redux";
 import get from "utils/server/get";
 import resolveReason from "utils/server/resolveReason";
 
 const useServerSites = (
 	onSuccess = sites => null,
-	onError = error => null,
+	onError   = error => null,
 ) => {
-	const store = useStore();
+	const store    = useStore();
+	const navigate = useNavigate();
 	useEffect(() => {
 		const cancelToken = axios.CancelToken.source();
-		get(selectLink("root.server.sites", store.getState()), onSuccess, onError, cancelToken);
+		get(selectLink("root.server.sites", store.getState()), onSuccess, onError, cancelToken, resolveReason(null, navigate));
 		return () => cancelToken.cancel();
 		// eslint-disable-next-line
 	}, [store]);
@@ -21,10 +22,10 @@ const useServerSites = (
 
 const useServerValidate = (
 	onSuccess = validation => null,
-	onError = error => null,
-	onReason = null,
+	onError   = error => null,
+	onReason  = null,
 ) => {
-	const store = useStore();
+	const store    = useStore();
 	const navigate = useNavigate();
 	useEffect(() => {
 		const cancelToken = axios.CancelToken.source();
