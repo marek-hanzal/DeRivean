a0b49ce15a5aa24193c4c9e9978dc9609583559e
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-09 14:04:02 +0100

    Client: Fixed some hidden shit

--- client/src/site/root/view/common/CommonHomeView.jsx
+++ client/src/site/root/view/common/CommonHomeView.jsx
@@ -1,51 +1,42 @@
-import {
-	Button,
-	Card,
-	Divider,
-	Result
-} from "antd";
+import {Button, Card, Divider, Result} from "antd";
 import EditIcon from "component/icon/EditIcon";
 import Spinner from "component/icon/Spinner";
 import Placeholder from "component/Placeholder";
 import BackLink from "component/route/BackLink";
 import useMenuSelect from "hook/useMenuSelect";
 import PropTypes from "prop-types";
-import {
-	createElement,
-	useContext,
-	useState
-} from "react";
-import { useTranslation } from "react-i18next";
-import { useDispatch } from "react-redux";
-import {
-	useNavigate,
-	useParams
-} from "react-router";
-import { NavigationRedux } from "redux/navigation/redux";
+import {createElement, useContext, useState} from "react";
+import {useTranslation} from "react-i18next";
+import {useDispatch} from "react-redux";
+import {useNavigate, useParams} from "react-router";
+import {NavigationRedux} from "redux/navigation/redux";
 
-const ContextView = ({
-						 context,
-						 fetch,
-						 navigation,
-						 param,
-						 menu,
-						 children
-					 }) => {
-	const dispatch              = useDispatch();
-	const {t}                   = useTranslation();
-	const params                = useParams();
+const ContextView = (
+	{
+		context,
+		fetch,
+		navigation,
+		param,
+		menu,
+		children
+	}) => {
+	const dispatch = useDispatch();
+	const {t} = useTranslation();
+	const params = useParams();
 	const [loading, setLoading] = useState(true);
-	const [data, setData]       = useState();
-	const navigate              = useNavigate();
-	const currentContext        = useContext(context);
+	const [data, setData] = useState();
+	const navigate = useNavigate();
+	const currentContext = useContext(context);
+	param = currentContext.param || param;
 	useMenuSelect([menu]);
 	fetch(
-		params[currentContext.param || param],
+		params[param],
 		data => {
 			setData(data);
 			setLoading(false);
 			dispatch(NavigationRedux.params(navigation(data)));
-		}, () => {
+		},
+		() => {
 			setLoading(false);
 		}
 	);
@@ -58,7 +49,7 @@ const ContextView = ({
 					<>
 						<Button type={"primary"} size={"large"} icon={<EditIcon/>} ghost onClick={() => {
 							navigate(currentContext.link.edit.link(params[param]));
-						}} children={t("root.hero.edit.form.edit")}/>
+						}} children={t("root.edit.form.edit")}/>
 						<Divider type={"horizontal"}/>
 					</>
 				}
@@ -71,10 +62,11 @@ const ContextView = ({
 	);
 };
 
-const CommonHomeView = ({
-							base,
-							...props
-						}) => {
+const CommonHomeView = (
+	{
+		base,
+		...props
+	}) => {
 	return (
 		createElement(
 			base,
@@ -85,12 +77,12 @@ const CommonHomeView = ({
 };
 
 CommonHomeView.propTypes = {
-	base:       PropTypes.func.isRequired,
-	context:    PropTypes.object.isRequired,
-	fetch:      PropTypes.func.isRequired,
+	base: PropTypes.func.isRequired,
+	context: PropTypes.object.isRequired,
+	fetch: PropTypes.func.isRequired,
 	navigation: PropTypes.func.isRequired,
-	children:   PropTypes.func.isRequired,
-	menu:       PropTypes.string.isRequired,
+	children: PropTypes.func.isRequired,
+	menu: PropTypes.string.isRequired,
 };
 
 export default CommonHomeView;
