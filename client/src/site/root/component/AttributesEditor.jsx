52b371dc5a30c12699f719c276c3055a6d766019
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-10 12:00:22 +0100

    Client: Fixed and simplified attribute editor(s)

--- client/src/site/root/component/AttributesEditor.jsx
+++ client/src/site/root/component/AttributesEditor.jsx
@@ -1,25 +1,23 @@
-import {
-	Card,
-	Divider,
-	Result
-} from "antd";
+import {Card, Divider, message, Result} from "antd";
+import BaseEditor from "component/form/BaseEditor";
 import EditorContext from "component/form/EditorContext";
 import EditorToolbar from "component/form/EditorToolbar";
 import AttributeIcon from "component/icon/AttributeIcon";
 import Spinner from "component/icon/Spinner";
 import Centered from "component/layout/Centered";
 import BackLink from "component/route/BackLink";
-import { useContext } from "react";
-import { useTranslation } from "react-i18next";
-import { useParams } from "react-router";
+import {useContext} from "react";
+import {useTranslation} from "react-i18next";
+import {useDispatch, useStore} from "react-redux";
+import {useParams} from "react-router";
+import {LoadingRedux} from "redux/loading/redux";
 import AttributeFieldEditor from "site/root/component/AttributeFieldEditor";
 import values from "utils/form/values";
 
-const AttributesEditor = ({context}) => {
-	const {t}            = useTranslation();
-	const currentContext = useContext(context);
-	const editorContext  = useContext(EditorContext);
-	const params         = useParams();
+const AttributeForm = ({currentContext}) => {
+	const {t} = useTranslation();
+	const editorContext = useContext(EditorContext);
+	const params = useParams();
 	currentContext.fetch(params[currentContext.param], fetch => {
 		editorContext.setInitials(fetch);
 		values(editorContext.form, fetch);
@@ -32,7 +30,7 @@ const AttributesEditor = ({context}) => {
 				icon={<Spinner icon={<AttributeIcon/>} done={!editorContext.ready}/>}
 				title={
 					<>
-						<EditorToolbar param={currentContext.param} redux={currentContext.redux} translation={currentContext.id}/>
+						<EditorToolbar param={currentContext.param} currentContext={currentContext}/>
 						<Divider type={"horizontal"}/>
 					</>
 				}
@@ -44,6 +42,40 @@ const AttributesEditor = ({context}) => {
 				}
 			/>
 		</Card>
+	);
+};
+
+const AttributesEditor = ({currentContext}) => {
+	const dispatch = useDispatch();
+	const {t} = useTranslation();
+	const store = useStore();
+	return (
+		<BaseEditor
+			readyCount={2}
+			onFinish={(values, initials, editor) => {
+				dispatch(LoadingRedux.start());
+				values = {id: initials.id, ...values};
+				currentContext.update(
+					store.getState(),
+					values,
+					() => {
+						message.success(t(`${currentContext.id}.attributes.updated`));
+						editor.setEditor(false);
+						editor.setInitials(values);
+						dispatch(LoadingRedux.finish());
+					},
+					() => {
+						message.success(t(`${currentContext.id}.attributes.update-failed`));
+						dispatch(LoadingRedux.finish());
+					}
+				);
+			}}
+			name={currentContext.id}
+			translation={currentContext.id}
+			children={
+				<AttributeForm currentContext={currentContext}/>
+			}
+		/>
 	);
 };
 
--- client/src/site/root/module/hero/view/edit/AttributesView.jsx
+++ client/src/site/root/module/hero/view/edit/AttributesView.jsx
@@ -1,34 +1,15 @@
-import {message} from "antd";
-import BaseEditor from "component/form/BaseEditor";
 import useMenuSelect from "hook/useMenuSelect";
-import {useTranslation} from "react-i18next";
-import {useStore} from "react-redux";
-import {doHeroUpdate} from "redux/hero/redux";
 import AttributesEditor from "site/root/component/AttributesEditor";
 import HeroContext from "site/root/module/hero/component/HeroContext";
 import HeroView from "site/root/module/hero/view/HeroView";
 
 const AttributesView = () => {
-	const store = useStore();
-	const {t} = useTranslation();
 	useMenuSelect(["root.hero.attributes"]);
 	return (
 		<HeroView>
 			<HeroContext.Consumer>
-				{({id}) => (
-					<BaseEditor
-						readyCount={2}
-						onFinish={(values, initials) => {
-							doHeroUpdate(
-								store.getState(),
-								{id: initials.id, ...values},
-								() => message.success(t(`${id}.attributes.updated`)),
-							);
-						}}
-						name={id}
-						translation={id}
-						children={<AttributesEditor context={HeroContext}/>}
-					/>
+				{(currentContext) => (
+					<AttributesEditor currentContext={currentContext}/>
 				)}
 			</HeroContext.Consumer>
 		</HeroView>
