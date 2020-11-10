a12e09afb3610a9ab0a09522af1732a7a8fa9700
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-10 12:13:24 +0100

    Client: Removed client redux

--- client/src/redux/client/redux.js
+++ client/src/redux/client/redux.js
@@ -1,55 +1,32 @@
+import axios from "axios";
 import {useEffect} from "react";
 import {useDispatch} from "react-redux";
-import {LoadingRedux} from "redux/loading/redux";
-import {Server} from "server";
-import fetchActions from "utils/action/actions/fetchActions";
-import fetchReducer from "utils/action/fetchReducer";
-
-const actions = fetchActions("client");
-
-const branch = state => state.client;
-
-const ClientRedux = {
-	fetch: href => dispatch => {
-		dispatch(LoadingRedux.start());
-		dispatch(actions.request());
-		return Server.get(href)
-			.then(({data}) => {
-				dispatch(actions.success(data));
-				dispatch(LoadingRedux.finish());
-				return Promise.resolve(data);
-			})
-			.catch(({response}) => {
-				dispatch(actions.failure(response.data));
-				dispatch(LoadingRedux.finish());
-				return Promise.reject(response.data);
-			});
-	},
-	reducer: () => fetchReducer(actions),
-};
-
-/**
- * return current backend discovery url
- *
- * @param state
- * @returns {string}
- */
-const selectDiscoveryLink = state => branch(state).payload.discovery;
+import {useNavigate} from "react-router";
+import get from "utils/server/get";
+import resolveReason from "utils/server/resolveReason";
 
 const useClient = (
-	onSuccess = data => ({}),
-	onFailure = data => ({}),
+	onSuccess = data => null,
+	onError = null,
+	onReason = null,
 	href = "/client.json"
 ) => {
 	const dispatch = useDispatch();
+	const navigate = useNavigate();
 	useEffect(() => {
-		dispatch(ClientRedux.fetch(href)).then(onSuccess, onFailure);
+		const cancelToken = axios.CancelToken.source();
+		get(
+			href,
+			onSuccess,
+			onError,
+			cancelToken,
+			resolveReason(onReason, navigate),
+		);
+		return () => cancelToken.cancel();
 		// eslint-disable-next-line
 	}, [dispatch, href]);
 };
 
 export {
-	ClientRedux,
 	useClient,
-	selectDiscoveryLink,
 };
