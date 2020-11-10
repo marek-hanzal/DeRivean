21ef5b547573bdaa514e6d52c6d13a3442dfc5f3
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-10 16:57:58 +0100

    Server: Added server side logout support

--- src/main/kotlin/derivean/server/auth/TicketService.kt
+++ src/main/kotlin/derivean/server/auth/TicketService.kt
@@ -2,13 +2,18 @@ package derivean.server.auth
 
 import derivean.lib.container.AbstractService
 import derivean.lib.container.IContainer
+import derivean.server.user.UserRepository
 import derivean.server.user.entities.User
 import java.util.*
 
 class TicketService(container: IContainer) : AbstractService(container) {
+	private val userRepository: UserRepository by container.lazy()
+
 	fun ticketFor(user: User): UUID = UUID.randomUUID().also { user.ticket = it }
 
 	fun drop(user: User) {
 		user.ticket = null
 	}
+
+	fun drop(ticket: UUID) = drop(userRepository.findByTicket(ticket))
 }
