e8311e599c3097413c87e611de88f1c325763bd4
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-11 16:57:33 +0100

    Client: Next bugfix

--- client/src/component/layout/CollapsibleContent.jsx
+++ client/src/component/layout/CollapsibleContent.jsx
@@ -1,16 +1,17 @@
 import {Layout} from "antd";
+import {LayoutContext} from "component/layout/BaseLayout";
+import {useContext} from "react";
 import {useSelector} from "react-redux";
-import {ContentRedux} from "redux/content/redux";
 import {MenuRedux} from "redux/menu/redux";
 
 const CollapsibleContent = ({children}) => {
 	const isCollapsed = useSelector(MenuRedux.selector.isCollapsed);
-	const isFullsize = useSelector(ContentRedux.selector.isFullsize);
+	const layoutContext = useContext(LayoutContext);
 	return (
 		<Layout.Content
 			style={{
 				minHeight: "100vh",
-				marginLeft: isFullsize ? 0 : (isCollapsed ? 80 : 220),
+				marginLeft: layoutContext.fullscreen ? 0 : (isCollapsed ? 80 : 220),
 			}}
 			children={children}
 		/>
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,11 +1,9 @@
 import {combineReducers} from "redux";
-import {ContentRedux} from "redux/content/redux";
 import {LoadingRedux} from "redux/loading/redux";
 import {MenuRedux} from "redux/menu/redux";
 import {NavigationRedux} from "redux/navigation/redux";
 
 export default combineReducers({
-	content: ContentRedux.reducer(),
 	loading: LoadingRedux.reducer(),
 	menu: MenuRedux.reducer(),
 	navigation: NavigationRedux.reducer(),
