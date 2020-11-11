2ea7c305bfa551ffbe5b6d138ba4037bd9d9f608
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-11 16:44:44 +0100

    Client: Prepared base layout context

--- client/src/component/layout/BaseLayout.jsx
+++ client/src/component/layout/BaseLayout.jsx
@@ -2,6 +2,9 @@ import {Layout, PageHeader} from "antd";
 import CollapsibleContent from "component/layout/CollapsibleContent";
 import Sider from "component/layout/Sider";
 import Loader from "component/Loader";
+import React, {useState} from "react";
+
+const LayoutContext = React.createContext(null);
 
 const BaseLayout = (
 	{
@@ -11,24 +14,33 @@ const BaseLayout = (
 		footer,
 	}
 ) => {
+	const [fullscreen, setFullscreen] = useState(false);
 	return (
-		<Loader>
-			<Layout>
-				{header}
-				<Layout style={{
-					minHeight: "100vh",
-					padding: "0 50px",
-					marginTop: 64
-				}}>
-					<Sider children={menu}/>
-					<CollapsibleContent>
-						<PageHeader/>
-						{router}
-						{footer}
-					</CollapsibleContent>
-				</Layout>
-			</Layout>
-		</Loader>
+		<LayoutContext.Provider
+			value={{
+				fullscreen,
+				setFullscreen,
+			}}
+			children={
+				<Loader>
+					<Layout>
+						{header}
+						<Layout style={{
+							minHeight: "100vh",
+							padding: "0 50px",
+							marginTop: 64
+						}}>
+							<Sider children={menu}/>
+							<CollapsibleContent>
+								<PageHeader/>
+								{router}
+								{footer}
+							</CollapsibleContent>
+						</Layout>
+					</Layout>
+				</Loader>
+			}
+		/>
 	);
 };
 
