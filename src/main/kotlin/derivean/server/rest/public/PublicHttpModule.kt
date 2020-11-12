e12d4eb2e898ee624a29e7f71b02b3abb03d7aa9
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-12 20:03:26 +0100

    Server: Some dark magic appears, it's time to move piece by piece :|

--- src/main/kotlin/derivean/server/rest/public/PublicHttpModule.kt
+++ src/main/kotlin/derivean/server/rest/public/PublicHttpModule.kt
@@ -5,7 +5,9 @@ import derivean.lib.http.AbstractHttpModule
 import derivean.server.rest.public.common.CommonHttpModule
 import derivean.server.rest.public.user.UserHttpModule
 import io.ktor.routing.*
+import io.ktor.util.*
 
+@KtorExperimentalAPI
 class PublicHttpModule(container: IContainer) : AbstractHttpModule(container) {
 	override fun install(routing: Routing) {
 		modules(
