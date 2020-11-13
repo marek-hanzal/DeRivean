0e80077285c9b367d2794bd4b33c5dc3daad698c
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-13 12:38:44 +0100

    Server: Another little cleanup

--- src/main/kotlin/derivean/server/attribute/Utils.kt
+++
@@ -1,6 +0,0 @@
-package derivean.server.attribute
-
-import derivean.server.attribute.entities.Attribute
-import org.jetbrains.exposed.sql.SizedIterable
-
-fun SizedIterable<Attribute>.valueOf(name: String) = this.firstOrNull { attribute -> attribute.name == name }?.value
