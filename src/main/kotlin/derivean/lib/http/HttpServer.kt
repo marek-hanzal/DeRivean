beacfb0286bc43379ff8013f43fcbb39afe6f70f
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-13 14:40:36 +0100

    Server: Fixed missing usage of templates

--- src/main/kotlin/derivean/lib/http/HttpServer.kt
+++ src/main/kotlin/derivean/lib/http/HttpServer.kt
@@ -15,8 +15,10 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.sessions.*
 import io.ktor.util.*
+import kotlinx.coroutines.delay
 import mu.KotlinLogging
 import java.util.*
+import kotlin.random.Random
 import kotlin.reflect.KClass
 
 @KtorExperimentalAPI
@@ -87,9 +89,9 @@ class HttpServer(container: IContainer) : AbstractConfigurable(), IHttpServer {
 			/**
 			 * Slow server emulation
 			 */
-//			intercept(ApplicationCallPipeline.Features) {
-//				delay(Random.nextLong(150, 350))
-//			}
+			intercept(ApplicationCallPipeline.Features) {
+				delay(Random.nextLong(250, 850))
+			}
 			modules.forEach {
 				logger.debug { "Setup: Installing module [${it.qualifiedName}]" }
 				routing {
--- src/main/kotlin/derivean/server/kingdom/KingdomRepository.kt
+++ src/main/kotlin/derivean/server/kingdom/KingdomRepository.kt
@@ -1,8 +1,10 @@
 package derivean.server.kingdom
 
+import derivean.game.attribute.Attribute
 import derivean.lib.container.IContainer
 import derivean.lib.repository.AbstractRepository
 import derivean.lib.storage.ilike
+import derivean.server.attribute.AttributeRepository
 import derivean.server.kingdom.entities.Kingdom
 import derivean.server.kingdom.entities.KingdomTable
 import org.jetbrains.exposed.sql.SortOrder
@@ -10,6 +12,8 @@ import org.jetbrains.exposed.sql.or
 import java.util.*
 
 class KingdomRepository(container: IContainer) : AbstractRepository<Kingdom, KingdomTable>(Kingdom, KingdomTable, container) {
+	private val attributeRepository: AttributeRepository by container.lazy()
+
 	fun findByName(name: String) = entity.find { table.name eq name }.first()
 
 	fun search(search: String, limit: Int = 100) = try {
@@ -24,6 +28,6 @@ class KingdomRepository(container: IContainer) : AbstractRepository<Kingdom, Kin
 	fun useTemplate(template: String, target: Kingdom) = useTemplate(search(template).first(), target)
 
 	fun useTemplate(kingdom: Kingdom, target: Kingdom) {
-//		attributes(target.id, kingdom.attributes.map { Attribute(it.name, it.value) }.toTypedArray(), replace = false)
+		attributeRepository.attributes(target.attributes, kingdom.attributes.map { Attribute(it.name, it.value) }.toTypedArray(), replace = false)
 	}
 }
