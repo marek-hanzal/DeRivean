412de4e58329ba585959c27c6589737e1bf15bd4
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-15 19:11:08 +0100

    Server: Ooouch, dat attribute stuff is wrooong!

--- src/main/kotlin/derivean/server/upgrade/u2020_11_15/entities/UpgradeAttributeHeroTable.kt
+++
@@ -1,9 +0,0 @@
-package derivean.server.upgrade.u2020_11_15.entities
-
-import org.jetbrains.exposed.dao.id.UUIDTable
-import org.jetbrains.exposed.sql.ReferenceOption
-
-object UpgradeAttributeHeroTable : UUIDTable("attribute-hero") {
-	val hero = reference("hero", UpgradeHeroTable, ReferenceOption.CASCADE, ReferenceOption.CASCADE)
-	val attribute = reference("attribute", UpgradeAttributeTable, ReferenceOption.CASCADE, ReferenceOption.CASCADE)
-}
