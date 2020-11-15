c9b9cec21fd358d0528964d8b800d77fe14cc019
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-15 19:11:08 +0100

    Server: Ooouch, dat attribute stuff is wrooong!

--- src/main/kotlin/derivean/server/upgrade/u2020_11_15/u2020_11_15.kt
+++ src/main/kotlin/derivean/server/upgrade/u2020_11_15/u2020_11_15.kt
@@ -2,7 +2,10 @@ package derivean.server.upgrade.u2020_11_15
 
 import derivean.lib.container.IContainer
 import derivean.lib.upgrade.AbstractUpgrade
-import derivean.server.upgrade.u2020_11_15.entities.*
+import derivean.server.upgrade.u2020_11_15.entities.UpgradeAttributeTable
+import derivean.server.upgrade.u2020_11_15.entities.UpgradeBuilding
+import derivean.server.upgrade.u2020_11_15.entities.UpgradeBuildingTable
+import derivean.server.upgrade.u2020_11_15.entities.UpgradeBuildingTableOpen
 import org.jetbrains.exposed.sql.SchemaUtils
 
 class u2020_11_15(container: IContainer) : AbstractUpgrade(container) {
@@ -11,8 +14,6 @@ class u2020_11_15(container: IContainer) : AbstractUpgrade(container) {
 			SchemaUtils.createMissingTablesAndColumns(
 				UpgradeAttributeTable,
 				UpgradeBuildingTableOpen,
-				UpgradeAttributeBuildingTable,
-				UpgradeAttributeHeroTable,
 			)
 		}
 		storage.write {
