be25c7ef6da6a25e7b72b6b5677943c8616b4985
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-16 17:54:41 +0100

    Client: Removed attribute editor from kingdom create view

--- client/src/site/root/module/kingdom/view/create/CreateView.jsx
+++ client/src/site/root/module/kingdom/view/create/CreateView.jsx
@@ -1,8 +1,7 @@
-import {Card, Divider} from "antd";
+import {Card} from "antd";
 import ModuleContext from "component/ModuleContext";
 import {useTranslation} from "react-i18next";
 import CommonCreateView from "site/common/view/CommonCreateView";
-import AttributeFieldEditor from "site/root/component/AttributeFieldEditor";
 import TemplateInput from "site/root/module/kingdom/component/form/TemplateInput";
 import KingdomView from "site/root/module/kingdom/view/KingdomView";
 
@@ -15,10 +14,8 @@ const CreateView = () => {
 					<CommonCreateView
 						param={parent}
 						defaultEnableSubmit={true}
-						readyCount={2}
+						readyCount={1}
 					>
-						<AttributeFieldEditor/>
-						<Divider type={"horizontal"}/>
 						<Card title={t(`${id}.create.form.misc.title`)}>
 							<TemplateInput/>
 						</Card>
