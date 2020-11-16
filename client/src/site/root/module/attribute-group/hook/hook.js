9e29937f3aecfbb8696e20c939b201125d7839ca
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-16 20:12:12 +0100

    Client: Enabled initial attribute editation in kingdom

--- client/src/site/root/module/attribute-group/hook/hook.js
+++ client/src/site/root/module/attribute-group/hook/hook.js
@@ -1,7 +1,9 @@
 import commonFetchHook from "utils/hook/commonFetchHook";
 
 const useAttributeGroupFetch = commonFetchHook("root.attribute-group.fetch");
+const useAttributeGroupNameFetch = commonFetchHook("root.attribute-group.attribute-type.list", "{name}");
 
 export {
 	useAttributeGroupFetch,
+	useAttributeGroupNameFetch,
 };
--- client/src/site/root/module/kingdom/view/attributes/CommonView.jsx
+++ client/src/site/root/module/kingdom/view/attributes/CommonView.jsx
@@ -1,6 +1,7 @@
 import {LayoutContext} from "component/layout/BaseLayout";
 import {useContext} from "react";
 import AttributesEditor from "site/root/component/AttributesEditor";
+import {useAttributeGroupNameFetch} from "site/root/module/attribute-group/hook/hook";
 import KingdomView from "site/root/module/kingdom/view/KingdomView";
 
 const CommonView = () => {
@@ -8,7 +9,12 @@ const CommonView = () => {
 	layoutContext.useMenuSelect(["root.kingdom.attributes.common"]);
 	return (
 		<KingdomView>
-			<AttributesEditor/>
+			<AttributesEditor
+				useAttributeFetch={(_, events) => {
+					// eslint-disable-next-line
+					useAttributeGroupNameFetch("kingdoms", events);
+				}}
+			/>
 		</KingdomView>
 	);
 };
