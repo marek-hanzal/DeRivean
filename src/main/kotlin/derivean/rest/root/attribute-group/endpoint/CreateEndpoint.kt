39fc81f0f560e7089a4d806434874a0ed9829051
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-16 13:26:12 +0100

    Server: Added rest of endpoints for attribute group

---
+++ src/main/kotlin/derivean/rest/root/attribute-group/endpoint/CreateEndpoint.kt
@@ -0,0 +1,55 @@
+package derivean.rest.root.`attribute-group`.endpoint
+
+import derivean.lib.container.IContainer
+import derivean.lib.http.withAnyRole
+import derivean.lib.mapper.AbstractCreateMapper
+import derivean.lib.rest.AbstractActionEndpoint
+import derivean.lib.rest.ApplicationRequest
+import derivean.lib.rest.Response
+import derivean.storage.entities.AttributeGroup
+import derivean.storage.repository.AttributeGroupRepository
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.routing.*
+import io.ktor.util.*
+
+@KtorExperimentalAPI
+class CreateEndpoint(container: IContainer) : AbstractActionEndpoint(container) {
+	private val createMapper: CreateMapper by container.lazy()
+
+	override fun install(routing: Routing) {
+		"/api/root/attribute-group/create".let { url ->
+			discovery {
+				name = "root.attribute-group.create"
+				link = url
+				description = "Creates a new Attribute Group"
+			}
+			routing.authenticate {
+				withAnyRole("root") {
+					post(url) {
+						resolve(call, createMapper)
+					}
+				}
+			}
+		}
+	}
+}
+
+class CreateMapper(container: IContainer) : AbstractCreateMapper<ApplicationRequest<CreateMapper.Request>, AttributeGroup>(container) {
+	override val repository: AttributeGroupRepository by container.lazy()
+	override val fetchMapper: FetchMapper by container.lazy()
+
+	override fun map(request: ApplicationRequest<Request>, entity: AttributeGroup) {
+		request.request.let {
+			entity.name = it.name
+			entity.description = it.description
+		}
+	}
+
+	override fun resolveException(message: String): Response<out Any>? = null
+
+	data class Request(
+		val name: String,
+		val description: String?,
+	)
+}
---
+++ src/main/kotlin/derivean/rest/root/attribute-group/endpoint/DeleteEndpoint.kt
@@ -0,0 +1,50 @@
+package derivean.rest.root.`attribute-group`.endpoint
+
+import derivean.lib.container.IContainer
+import derivean.lib.http.withAnyRole
+import derivean.lib.mapper.AbstractActionMapper
+import derivean.lib.rest.*
+import derivean.storage.repository.AttributeGroupRepository
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.routing.*
+import io.ktor.util.*
+
+@KtorExperimentalAPI
+class DeleteEndpoint(container: IContainer) : AbstractActionEndpoint(container) {
+	private val deleteMapper: DeleteMapper by container.lazy()
+
+	override fun install(routing: Routing) {
+		"/api/root/attribute-group/delete".let { url ->
+			discovery {
+				name = "root.attribute-group.delete"
+				link = url
+				description = "Delete a Attribute Group"
+			}
+			routing.authenticate {
+				withAnyRole("root") {
+					post(url) {
+						resolve(call, deleteMapper)
+					}
+				}
+			}
+		}
+	}
+}
+
+class DeleteMapper(container: IContainer) : AbstractActionMapper<ApplicationRequest<DeleteMapper.Request>, Response<out Any>>(container) {
+	private val attributeGroupRepository: AttributeGroupRepository by container.lazy()
+
+	override fun resolve(item: ApplicationRequest<Request>) = try {
+		ok(storage.write {
+			attributeGroupRepository.delete(item.request.id)
+		})
+	} catch (e: Throwable) {
+		logger.error(e.message, e)
+		internalServerError(ValidationResponse.build {
+			message = "Some ugly internal server error happened!"
+		})
+	}
+
+	data class Request(val id: String)
+}
