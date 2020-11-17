98e81e42638e4790eb9ed972278f364ff64ab5c4
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-17 14:54:07 +0100

    Server: Fixed validator service

--- src/main/kotlin/derivean/server/user/SessionValidator.kt
+++ src/main/kotlin/derivean/server/user/SessionValidator.kt
@@ -2,15 +2,17 @@ package derivean.server.user
 
 import derivean.lib.container.AbstractService
 import derivean.lib.container.IContainer
+import derivean.lib.storage.IStorage
 import derivean.lib.user.ISessionValidator
 import derivean.lib.user.SessionTicket
 import derivean.storage.repository.UserRepository
 
 class SessionValidator(container: IContainer) : AbstractService(container), ISessionValidator {
 	private val userRepository: UserRepository by container.lazy()
+	private val storage: IStorage by container.lazy()
 
 	override fun validate(sessionTicket: SessionTicket) = try {
-		userRepository.findByTicket(sessionTicket.id)
+		storage.read { userRepository.findByTicket(sessionTicket.id) }
 		sessionTicket
 	} catch (e: NoSuchElementException) {
 		logger.error(e.message, e)
