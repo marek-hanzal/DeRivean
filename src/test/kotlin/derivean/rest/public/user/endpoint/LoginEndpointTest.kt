959770fbfb58abea845112976210906202d63eca
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-19 11:46:53 +0100

    Server: Cool - very first http test!

--- src/test/kotlin/derivean/rest/public/user/endpoint/LoginEndpointTest.kt
+++ src/test/kotlin/derivean/rest/public/user/endpoint/LoginEndpointTest.kt
@@ -2,10 +2,16 @@ package derivean.rest.public.user.endpoint
 
 import derivean.TestContainer
 import derivean.rest.HttpClient
+import io.ktor.client.call.*
+import io.ktor.client.features.*
+import io.ktor.http.*
 import io.ktor.util.*
+import kotlinx.coroutines.runBlocking
 import leight.http.IHttpServer
+import leight.rest.ValidationResponse
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 @KtorExperimentalAPI
 @ExperimentalStdlibApi
@@ -20,6 +26,23 @@ class LoginEndpointTest {
 				assertEquals("game", login.login)
 				assertEquals("The Gamer", login.name)
 				assertEquals("game", login.site)
+			}
+		}
+	}
+
+	@Test
+	fun `Login with bad credentials`() {
+		val container = TestContainer.setup()
+		val server = container.create(IHttpServer::class)
+		server.start("Testing Server", wait = false)
+		container.create(HttpClient::class).use { httpClient ->
+			assertFailsWith<ClientRequestException> {
+				httpClient.post<LoginEndpoint.Response>("/api/public/user/login", LoginEndpoint.Request("kaboom", "kaboom"))
+			}.let { exception ->
+				assertEquals(HttpStatusCode.Forbidden, exception.response.status)
+				runBlocking { exception.response.call.receive<ValidationResponse>() }.let { validationResponse ->
+					assertEquals("Login failed!", validationResponse.message)
+				}
 			}
 		}
 	}
