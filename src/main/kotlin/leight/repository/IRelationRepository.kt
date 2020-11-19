9236bfff1adf457378226867b97ae37814e11e77
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-19 16:30:50 +0100

    Server: Improved page support; now client-side filtering should work properly

--- src/main/kotlin/leight/repository/IRelationRepository.kt
+++ src/main/kotlin/leight/repository/IRelationRepository.kt
@@ -2,6 +2,7 @@ package leight.repository
 
 import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.id.UUIDTable
+import org.jetbrains.exposed.sql.SizedIterable
 import java.util.*
 
 interface IRelationRepository<T : UUIDEntity> {
@@ -9,6 +10,8 @@ interface IRelationRepository<T : UUIDEntity> {
 	 * Return total number of records in this repository (total number of rows in table).
 	 */
 	fun total(relation: UUID, filter: EntityFilter<T>? = null): Long
+
+	fun source(relation: UUID, paging: Paging): SizedIterable<T>
 
 	/**
 	 * Return just UUIDs of the given entity page.
