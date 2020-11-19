e7b2dc64dc540fe0aee9274b4801bc0a85456aea
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-19 11:19:38 +0100

    Server: Initial test of endpoint

--- src/test/kotlin/derivean/TestContainer.kt
+++ src/test/kotlin/derivean/TestContainer.kt
@@ -1,6 +1,9 @@
 package derivean
 
 import derivean.fixtures.Fixtures
+import io.ktor.client.*
+import io.ktor.client.features.cookies.*
+import io.ktor.client.features.json.*
 import io.ktor.util.*
 import leight.upgrade.IUpgradeManager
 
@@ -8,6 +11,16 @@ import leight.upgrade.IUpgradeManager
 @ExperimentalStdlibApi
 object TestContainer {
 	fun setup() = ServerContainer.create {
+		register(HttpClient::class) {
+			HttpClient {
+				install(HttpCookies) {
+					storage = AcceptAllCookiesStorage()
+				}
+				install(JsonFeature) {
+					serializer = GsonSerializer {}
+				}
+			}
+		}
 		configurator(IUpgradeManager::class) {
 			upgrade(Fixtures::class)
 		}
--- src/test/kotlin/derivean/rest/public/user/endpoint/LoginEndpointTest.kt
+++ src/test/kotlin/derivean/rest/public/user/endpoint/LoginEndpointTest.kt
@@ -1,9 +1,14 @@
 package derivean.rest.public.user.endpoint
 
 import derivean.TestContainer
+import io.ktor.client.*
+import io.ktor.client.request.*
 import io.ktor.util.*
+import kotlinx.coroutines.runBlocking
 import leight.http.IHttpServer
+import leight.http.ILinkGenerator
 import org.junit.Test
+import kotlin.test.assertEquals
 
 @KtorExperimentalAPI
 @ExperimentalStdlibApi
@@ -12,6 +17,18 @@ class LoginEndpointTest {
 	fun `Login with good credentials`() {
 		val container = TestContainer.setup()
 		val server = container.create(IHttpServer::class)
+		val linkGenerator = container.create(ILinkGenerator::class)
 		server.start("Testing Server", wait = false)
+		container.create(HttpClient::class).use {
+			runBlocking {
+				val login = it.post<LoginEndpoint.Response>(linkGenerator.link("/api/public/user/login")) {
+					header("Content-Type", "application/json")
+					body = LoginEndpoint.Request("game", "game")
+				}
+				assertEquals("game", login.login)
+				assertEquals("The Gamer", login.name)
+				assertEquals("game", login.site)
+			}
+		}
 	}
 }
