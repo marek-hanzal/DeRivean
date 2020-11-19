737a664c8c6c53266c1975c3febf2a3468253391
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-19 11:32:57 +0100

    Server: Improved client a bit

--- src/test/kotlin/derivean/TestContainer.kt
+++ src/test/kotlin/derivean/TestContainer.kt
@@ -1,9 +1,7 @@
 package derivean
 
 import derivean.fixtures.Fixtures
-import io.ktor.client.*
-import io.ktor.client.features.cookies.*
-import io.ktor.client.features.json.*
+import derivean.rest.HttpClient
 import io.ktor.util.*
 import leight.upgrade.IUpgradeManager
 
@@ -11,16 +9,7 @@ import leight.upgrade.IUpgradeManager
 @ExperimentalStdlibApi
 object TestContainer {
 	fun setup() = ServerContainer.create {
-		register(HttpClient::class) {
-			HttpClient {
-				install(HttpCookies) {
-					storage = AcceptAllCookiesStorage()
-				}
-				install(JsonFeature) {
-					serializer = GsonSerializer {}
-				}
-			}
-		}
+		register(HttpClient::class) { HttpClient(this) }
 		configurator(IUpgradeManager::class) {
 			upgrade(Fixtures::class)
 		}
