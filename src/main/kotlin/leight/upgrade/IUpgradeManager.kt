710957595c8835589687d81cbea3133ff080c634
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-19 10:50:30 +0100

    Server: Starting http server on dev

--- src/main/kotlin/leight/upgrade/IUpgradeManager.kt
+++ src/main/kotlin/leight/upgrade/IUpgradeManager.kt
@@ -7,7 +7,7 @@ interface IUpgradeManager : IConfigurable {
 	/**
 	 * register an upgrade (order of upgrades is important)
 	 */
-	fun <T : IUpgrade> register(upgrade: KClass<T>): IUpgradeManager
+	fun <T : IUpgrade> upgrade(upgrade: KClass<T>): IUpgradeManager
 
 	/**
 	 * execute upgrade process
