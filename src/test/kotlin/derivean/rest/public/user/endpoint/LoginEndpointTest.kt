bcf909b4c41a45586c6d3903c99cd9f75af0c8a1
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-19 10:42:28 +0100

    Server: Experimentally trying to prepare server test

---
+++ src/test/kotlin/derivean/rest/public/user/endpoint/LoginEndpointTest.kt
@@ -0,0 +1,17 @@
+package derivean.rest.public.user.endpoint
+
+import derivean.TestContainer
+import io.ktor.util.*
+import leight.http.IHttpServer
+import org.junit.Test
+
+@KtorExperimentalAPI
+@ExperimentalStdlibApi
+class LoginEndpointTest {
+	@Test
+	fun `Login with good credentials`() {
+		val container = TestContainer.setup()
+		val server = container.create(IHttpServer::class);
+		server.start("Testing Server", wait = false)
+	}
+}
